f0a733a830ec7fadb5b89053a8e787161d338ed4
Faneva Andriamiadantsoa <fanevanjanahary@gmail.com>
2023-06-01 00:13:54 +0300

    implement configure reminder

--- django_project/swaps/admin.py
+++ django_project/swaps/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from swaps.models import Reminder
+
 # Register your models here.
+
+
+class ReminderAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'text', 'status')
+    list_filter = ('title','status', 'date')
+    ordering = ('status', 'date')
+
+
+admin.site.register(Reminder, ReminderAdmin)
---
+++ django_project/swaps/models/__init__.py
@@ -0,0 +1,1 @@
+from swaps.models.reminder import *
