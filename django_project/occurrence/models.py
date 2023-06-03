0e257387fbd1571159cae4f6c4b4c64b9c992625
mohab <homab3@gmail.com>
2023-06-03 11:02:09 +0000

    organism quantity model docstring

--- django_project/occurrence/models.py
+++ django_project/occurrence/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class OrganismQuantityType(models.Model):
+    """organism quantity type model"""
     name = models.CharField(max_length=255, unique=True)
     sort_id = models.IntegerField(unique=True)
 
