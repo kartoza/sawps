f650a1067f59143ea6bfb65939ac8b9b766ba33d
mohab <homab3@gmail.com>
2023-06-05 06:52:34 +0000

    basis of record model

--- django_project/occurrence/factories.py
+++ django_project/occurrence/factories.py
@@ -1,12 +1,22 @@
 import factory
-import occurrence.models as OccurrenceModels
+from occurrence.models import SurveyMethod, BasisOfRecord
 
 
 class SurveyMethodFactory(factory.django.DjangoModelFactory):
     """survey method factory"""
 
     class Meta:
-        model = OccurrenceModels.SurveyMethod
+        model = SurveyMethod
 
     name = factory.Sequence(lambda n: 'survey method {0}'.format(n))
+    sort_id = factory.Sequence(lambda n: n)
+
+
+class BasisOfRecordFactory(factory.django.DjangoModelFactory):
+    """basis of record factory"""
+
+    class Meta:
+        model = BasisOfRecord
+
+    name = factory.Sequence(lambda n: 'basis of record {0}'.format(n))
     sort_id = factory.Sequence(lambda n: n)
