c213f4d76c8170ed4cfc8e2ea8296620c0f0d239
Faneva Andriamiadantsoa <fanevanjanahary@gmail.com>
2023-06-05 03:25:37 +0300

    Implement configure reminder (#412)
    * Update devcontainer (#363)
    
    * Update devcontainer (#363)
    
    adding pre-commit and black (#367)
    
    Update devcontainer (#363)
    
    adding pre-commit and black (#367)
    
    * implement configure reminder
    
    * move reminder to nofitcation app, add test
    
    * add reminder api
    
    * move api files into notification
    
    * remove unused code
    
    ---------
    
    Co-authored-by: Dimas Ciputra <dimas@kartoza.com>
    Co-authored-by: Faneva Andriamiadantsoa <fanevanjanahary@Fanevas-MacBook-Pro.local>

---
+++ django_project/notification/models/__init__.py
@@ -0,0 +1,1 @@
+from notification.models.reminder import *
---
+++ django_project/notification/tests/reminder_factory.py
@@ -0,0 +1,14 @@
+import factory
+from django.conf import settings
+from notification.models import Reminder
+
+
+class ReminderF(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Reminder
+        django_get_or_create = ('id', )
+
+    id = factory.Sequence(lambda n: n)
+    title = factory.Sequence(lambda n: 'title%s' % n)
+    text = factory.Sequence(lambda n: 'text%s' % n)
+    status = factory.Sequence(lambda n: 'status%s' % n)
