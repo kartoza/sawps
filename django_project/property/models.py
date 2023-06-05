cec9c0270ec65f501f915b1b35c3b3f30f99e539
mohab <homab3@gmail.com>
2023-06-05 18:38:40 +0000

    squash merge migrations

--- django_project/property/models.py
+++ django_project/property/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class PropertyType(models.Model):
     name = models.CharField(max_length=250, unique=True)
 
+    def __str__(self):
+        return self.name
+
     class META:
         verbose_name = 'Property type'
         verbose_name_plural = 'Property types'
