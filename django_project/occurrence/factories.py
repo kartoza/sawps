d84b1b4028d5bf0b268bdc73a3b0a8b66fae08ac
mohab <homab3@gmail.com>
2023-06-05 05:13:21 +0000

    adding sampling size units

--- django_project/occurrence/factories.py
+++ django_project/occurrence/factories.py
@@ -1,12 +1,21 @@
 import factory
-import occurrence.models as OccurrenceModels
+from occurrence.models import SurveyMethod, SamplingSizeUnit
 
 
 class SurveyMethodFactory(factory.django.DjangoModelFactory):
     """survey method factory"""
 
     class Meta:
-        model = OccurrenceModels.SurveyMethod
+        model = SurveyMethod
 
     name = factory.Sequence(lambda n: 'survey method {0}'.format(n))
     sort_id = factory.Sequence(lambda n: n)
+
+
+class SamplingSizeUnitFactory(factory.django.DjangoModelFactory):
+    """sampling size unit factory"""
+
+    class Meta:
+        model = SamplingSizeUnit
+
+    unit = factory.Faker('random_choices', elements='cm')
--- django_project/occurrence/test_occurrence_models.py
+++ django_project/occurrence/test_occurrence_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
-import occurrence.models as OccurrenceModels
-import occurrence.factories as OccurrenceFactories
+from occurrence.models import SurveyMethod, SamplingSizeUnit
+from occurrence.factories import SurveyMethodFactory, SamplingSizeUnitFactory
 from django.db.utils import IntegrityError
 
 
@@ -10,14 +10,14 @@ class SurveyMethodTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         """setup test data"""
-        cls.survey_method = OccurrenceFactories.SurveyMethodFactory()
+        cls.survey_method = SurveyMethodFactory()
 
     def test_create_survey_method(self):
         """test create survey method"""
         self.assertTrue(
-            isinstance(self.survey_method, OccurrenceModels.SurveyMethod)
+            isinstance(self.survey_method, SurveyMethod)
         )
-        self.assertEqual(OccurrenceModels.SurveyMethod.objects.count(), 1)
+        self.assertEqual(SurveyMethod.objects.count(), 1)
         self.assertEqual(self.survey_method.name, 'survey method 0')
 
     def test_update_survey_method(self):
@@ -25,7 +25,7 @@ class SurveyMethodTestCase(TestCase):
         self.survey_method.name = 'survey method 1'
         self.survey_method.save()
         self.assertEqual(
-            OccurrenceModels.SurveyMethod.objects.get(id=1).name,
+            SurveyMethod.objects.get(id=1).name,
             'survey method 1',
         )
 
@@ -44,4 +44,36 @@ class SurveyMethodTestCase(TestCase):
     def test_delete_survey_method(self):
         """test delete survey method"""
         self.survey_method.delete()
-        self.assertEqual(OccurrenceModels.SurveyMethod.objects.count(), 0)
+        self.assertEqual(SurveyMethod.objects.count(), 0)
+
+
+class SamplingSizeUnitTestCase(TestCase):
+    """sampling size unit testcase"""
+
+    @classmethod
+    def setUpTestData(cls):
+        """setup test data"""
+        cls.sampling_size_unit = SamplingSizeUnitFactory(unit='cm')
+
+    def test_create_sampling_size_unit(self):
+        """test create sampling size unit"""
+        self.assertTrue(isinstance(self.sampling_size_unit, SamplingSizeUnit))
+        self.assertEqual(SamplingSizeUnit.objects.count(), 1)
+        self.assertEqual(self.sampling_size_unit.unit, 'cm')
+
+    def test_update_sampling_size_unit(self):
+        """test update sampling size unit"""
+        self.sampling_size_unit.unit = 'mm'
+        self.sampling_size_unit.save()
+        self.assertEqual(SamplingSizeUnit.objects.get(id=1).unit, 'mm')
+
+    def test_sampling_size_unit_unique_unit_constraint(self):
+        """testing unique values for the unti"""
+        with self.assertRaises(Exception) as raised:
+            SamplingSizeUnitFactory(unit='mm')
+            self.assertEqual(raised.exception, IntegrityError)
+
+    def test_delete_sampling_size_unit(self):
+        """test delete sampling size unit"""
+        self.sampling_size_unit.delete()
+        self.assertEqual(SamplingSizeUnit.objects.count(), 0)
