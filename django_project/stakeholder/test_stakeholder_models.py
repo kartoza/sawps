0cb2573762c2b776aa869729d79acfc87f0c2a11
mohab <homab3@gmail.com>
2023-06-06 16:22:44 +0000

    user model

--- django_project/stakeholder/test_stakeholder_models.py
+++ django_project/stakeholder/test_stakeholder_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-from stakeholder.models import UserRoleType, UserTitle
-from stakeholder.factories import userRoleTypeFactory, userTitleFactory
+from stakeholder.models import UserRoleType, UserTitle, LoginStatus, UserProfile
+from stakeholder.factories import userRoleTypeFactory, userTitleFactory, loginStatusFactory, userFactory, userProfileFactory
+from django.contrib.auth.models import User
 
 
 class TestUserRoleType(TestCase):
@@ -55,3 +56,66 @@ class UserTitleTestCase(TestCase):
         """test deleting a title"""
         self.userTitle.delete()
         self.assertEqual(UserTitle.objects.count(), 0)
+
+
+class LoginStatusTestCase(TestCase):
+    """user login status test case"""
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.loginStatus = loginStatusFactory()
+
+    def test_create_login_status(self):
+        """test creating login status"""
+        self.assertEqual(LoginStatus.objects.count(), 1)
+        self.assertTrue(
+            isinstance(self.loginStatus, LoginStatus)
+        )
+        self.assertTrue(self.loginStatus.name in ['logged in', 'logged out'])
+
+    def test_update_login_status(self):
+        """test updating a login status"""
+        self.loginStatus.name = 'logged in'
+        loginStatus = LoginStatus.objects.get(
+            id=self.loginStatus.id
+        )
+        self.assertTrue(loginStatus.name, 'logged in')
+        self.loginStatus.name = 'logged out'
+        loginStatus = LoginStatus.objects.get(
+            id=self.loginStatus.id
+        )
+        self.assertTrue(loginStatus.name, 'logged out')
+
+    def test_delete_login_status(self):
+        """test deleting a login status"""
+        self.loginStatus.delete()
+        self.assertEqual(LoginStatus.objects.count(), 0)
+
+
+class TestUser(TestCase):
+    """test the main user model relation to the profie model"""
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.profileFactory = userProfileFactory()
+
+    def test_create_new_user_with_new_profile(self):
+        """test creating new user when new profile is created"""
+        self.assertEqual(UserProfile.objects.count(), 1)
+        self.assertEqual(
+            User.objects.count(), 2
+        )  # Anon user is created by default
+
+    def test_update_user_profile(self):
+        """test updating user through profile"""
+        self.profileFactory.user.username = 'test'
+        self.profileFactory.user.first_name = 'test123'
+        self.profileFactory.user.save()
+        self.assertEqual(
+            User.objects.get(username='test').first_name, 'test123'
+        )
+
+    def test_delete_role(self):
+        """test deleting user when a profile is deleted"""
+        self.profileFactory.delete()
+        self.assertEqual(User.objects.count(), 1)
