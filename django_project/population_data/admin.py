77f24599e5fe0b1bb163fcc6b4ee5cff780c8bde
Mohab <58084234+Mohab25@users.noreply.github.com>
2023-06-08 11:02:26 +0300

    adding month model - issue 393 (#453)
    * adding month model
    
    * merge migrations - resolve conflict on occurrence app
    
    * change month model fields

--- django_project/population_data/admin.py
+++ django_project/population_data/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from population_data.models import NatureOfPopulation
+from population_data.models import Month, NatureOfPopulation
 
+admin.site.register(Month)
 admin.site.register(NatureOfPopulation)
+
