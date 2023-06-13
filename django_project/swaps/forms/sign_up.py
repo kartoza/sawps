e30d9551fcf44cfd6404e77bb17e9b2ba3f9e5eb
Faneva Andriamiadantsoa <fanevanjanahary@gmail.com>
2023-06-13 13:10:20 +0300

    Fix issue on register (#503)
    * fix issue on register
    
    * remove used fields

--- django_project/swaps/forms/sign_up.py
+++ django_project/swaps/forms/sign_up.py
@@ -16,34 +16,18 @@ class CustomSignupForm(SignupForm):
     last_name = forms.CharField(
         max_length=150, label='Last Name', required=True
     )
-    organisation = forms.CharField(
-        max_length=100, label='Organisation or Entreprise name', required=True
-    )
-    group = forms.ModelChoiceField(
-        queryset=Group.objects.all(),
-        label='Role',
-        required=False,
-        widget=forms.Select(attrs={'class': 'form-control'}),
-    )
+    
 
     field_order = [
         'first_name',
         'last_name',
         'email',
-        'organisation',
-        'group',
         'password',
     ]
 
     def custom_signup(self, request, user):
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
-        user.organisation = self.cleaned_data['organisation']
-        try:
-            user.groups.set(Group.objects.get(id=self.cleaned_data['group']))
-        except Exception:
-            pass
-
         user.is_active = False
         user.save()
 
@@ -60,11 +44,7 @@ class CustomSignupForm(SignupForm):
         )
 
         user.email_user(subject, message)
-        messages.success(
-            request,
-            ('Please verify your email addresse to complete registration.'),
-        )
-
+        
         return user
 
     def __init__(self, *args, **kwargs):
