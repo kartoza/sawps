e04cd53b876d1698a83d717ff412b3fe51c38f89
Andriamiadantsoa <fanevanjanahary@gmail.com>
2023-06-13 19:53:58 +0300

    add passwor validation

--- django_project/swaps/forms/sign_up.py
+++ django_project/swaps/forms/sign_up.py
@@ -51,3 +51,4 @@ class CustomSignupForm(SignupForm):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = ''
