e0c8413ab9f15f69eb705d188fad41448a86683f
Danang <danangmassandy@gmail.com>
2023-06-14 05:40:51 +0000

    add property summary table

--- django_project/frontend/src/containers/MainPage/Property/PropertySummary.tsx
+++ django_project/frontend/src/containers/MainPage/Property/PropertySummary.tsx
@@ -1,15 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import {RootState} from '../../../app/store';
 import {useAppSelector } from '../../../app/hooks';
+import PropertySummaryTable from './PropertySummaryTable';
 
 
 export default function PropertySummary() {
@@ -37,69 +31,7 @@ export default function PropertySummary() {
                     <Grid item className='FlexContainerFillHeight'>
                         <Grid container className='SummaryContent'>
                             <Grid item>
-                                <TableContainer component={Paper}>
-                                    <Table className='PropertySiteDetailTable' aria-label="property site detail table" size='medium'>
-                                        <colgroup>
-                                            <col width="50%" />
-                                            <col width="50%" />
-                                        </colgroup>
-                                        <TableHead>
-                                            <TableRow>
-                                                <TableCell colSpan={2}>Site Details</TableCell>
-                                            </TableRow>
-                                        </TableHead>
-                                        <TableBody>
-                                            <TableRow key='property_code'>
-                                                <TableCell component="th" scope="row">
-                                                    Property Code
-                                                </TableCell>
-                                                <TableCell>
-                                                    { `ID${propertyItem.id}` }
-                                                </TableCell>
-                                            </TableRow>
-                                            <TableRow key='property_name'>
-                                                <TableCell component="th" scope="row">
-                                                    Property Name
-                                                </TableCell>
-                                                <TableCell>
-                                                    {propertyItem.name}
-                                                </TableCell>
-                                            </TableRow>
-                                            <TableRow key='owner_name'>
-                                                <TableCell component="th" scope="row">
-                                                    Owner Name
-                                                </TableCell>
-                                                <TableCell>
-                                                    {propertyItem.owner}
-                                                </TableCell>
-                                            </TableRow>
-                                            <TableRow key='property_type'>
-                                                <TableCell component="th" scope="row">
-                                                    Property Type
-                                                </TableCell>
-                                                <TableCell>
-                                                    {propertyItem.property_type}
-                                                </TableCell>
-                                            </TableRow>
-                                            <TableRow key='size'>
-                                                <TableCell component="th" scope="row">
-                                                    Property Size (in ha)
-                                                </TableCell>
-                                                <TableCell>
-                                                    {propertyItem.size}
-                                                </TableCell>
-                                            </TableRow>
-                                            <TableRow key='organisation'>
-                                                <TableCell component="th" scope="row">
-                                                    Organisation
-                                                </TableCell>
-                                                <TableCell>
-                                                    {propertyItem.organisation}
-                                                </TableCell>
-                                            </TableRow>
-                                        </TableBody>
-                                    </Table>
-                                </TableContainer>
+                                <PropertySummaryTable propertyItem={propertyItem} />
                             </Grid>
                             <Grid item>
                                 {/* Species data chart */}
