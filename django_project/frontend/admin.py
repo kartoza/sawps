44cc82e01518e6a76babff363bd9f804116f3f19
Danang <danangmassandy@gmail.com>
2023-06-21 05:23:49 +0000

    add admin listing page

--- django_project/frontend/admin.py
+++ django_project/frontend/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from frontend.models import ContextLayer
+from frontend.models import BoundaryFile, BoundarySearchRequest
+
+
+class BoundaryFileAdmin(admin.ModelAdmin):
+    list_display = ('meta_id', 'name', 'upload_date', 'session', 'file_type')
+
+
+class BoundarySearchRequestAdmin(admin.ModelAdmin):
+    list_display = ('session', 'type', 'status', 'progress')
 
 
 admin.site.register(ContextLayer)
+admin.site.register(BoundaryFile, BoundaryFileAdmin)
+admin.site.register(BoundarySearchRequest, BoundarySearchRequestAdmin)
