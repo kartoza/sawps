b8b7bbb14e2ad2985b80854bda3b4639f67056dd
dimasciput <dimas.ciputra@gmail.com>
2023-06-22 09:25:29 +0700

    Update landing page

--- django_project/frontend/src/components/LandingPage/LandingPagePopulationOverview/SpeciesCard/index.tsx
+++ django_project/frontend/src/components/LandingPage/LandingPagePopulationOverview/SpeciesCard/index.tsx
@@ -22,11 +22,13 @@ const SpeciesCard:FC<ISpeciesCardProps> =(props)=>{
             <div className='species-card-container' data-testid='species-card-container'>
                 <div className='species-card-image-container'>
                     <img src={props.pic} className='species-card-image' data-testid='species-card-image'/>
+                    <p className='species-card-text species-name-text'> {props.species_name} </p>
+                    <hr/>
                 </div>
                 <div className='species-card-text-container'>
-                    <p className='species-card-text' data-testid='species-card-population'>Total Population:{props.population}</p>
-                    <p className='species-card-text' data-testid='species-card-growth'>Population Growth:{props.growth}</p>
-                    <p className='species-card-text' data-testid='species-card-loss'>Population Loss:{props.loss}</p>
+                    <p className='species-card-text' data-testid='species-card-population'>Total Population : {props.population}</p>
+                    <p className='species-card-text' data-testid='species-card-growth'>Population Growth : {props.growth}</p>
+                    <p className='species-card-text' data-testid='species-card-loss'>Population Loss : {props.loss}</p>
                 </div>
                 <div className='ChartHolder'>
                 <SpeciesChart species_name={props.species_name} lineColor={props.chartColors.line} areaFillColor={props.chartColors.area}  index={props.index}/>
