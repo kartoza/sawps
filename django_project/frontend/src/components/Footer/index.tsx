e7e5f9ae7c0c4f936335f3e31bf5c02c8f52b34c
mohab <homab3@gmail.com>
2023-06-23 02:25:57 +0000

    adding tests to footer

--- django_project/frontend/src/components/Footer/index.tsx
+++ django_project/frontend/src/components/Footer/index.tsx
@@ -6,21 +6,22 @@ interface IFooter{}
 
 const Footer:FC<IFooter>=()=>{
     return (
-      <div className='footer'>
+      <div className='footer' data-testid='footer'>
           {/* Made with <img className='heart' src='/static/images/heart-icon.png'/> by&nbsp; 
           <a href='https://kartoza.com' target='_blank' style={{ color: "var(--white)"}}>Kartoza</a> */}
             <div className='sanbi-footer-logo-container'>
-                <img className='footer-logo' src='/static/images/footer/footer-logo.png'/>
+                <img className='footer-logo' data-testid='sanbi-footer-logo'
+                src='/static/images/footer/footer-logo.png'/>
             </div>
-            <div className='footer-nav'>
+            <div className='footer-nav' data-testid='footer-navigation'>
                 <a href='/' target='_blank'>HOME</a>
                 <a href='/map' target='_blank'>MAP</a>
-                <a href='/documentation' target='_blank'>DOCUMENTATION</a>
+                <a href='/' target='_blank'>DOCUMENTATION</a>
                 <a href='/contact' target='_blank'>CONTACT</a>
           </div>
-          <div className='vendors-logos-container'>
-            <img className='ids-logo' src='/static/images/footer/ids-logo.png'/>
-            <img className='kartoza-white-logo' src='/static/images/footer/kartoza-white.png'/>
+          <div className='vendors-logos-container' data-testid='vendors-logos-container'>
+            <img className='ids-logo' src='/static/images/footer/ids-logo.png' data-testid='ids-logo'/>
+            <img className='kartoza-white-logo' src='/static/images/footer/kartoza-white.png' data-testid='kartoza-logo'/>
           </div>
       </div>
     );
