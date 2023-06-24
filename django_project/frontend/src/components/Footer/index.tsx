eeb24c381c08333c2ce70fb20e1812854dc5b7b7
mohab <homab3@gmail.com>
2023-06-24 00:27:29 +0000

    adding footer

---
+++ django_project/frontend/src/components/Footer/index.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from 'react';
+import './index.scss';
+
+interface IFooter{}
+
+
+const Footer:FC<IFooter>=()=>{
+    return (
+      <div className='footer' data-testid='footer'>
+          {/* Made with <img className='heart' src='/static/images/heart-icon.png'/> by&nbsp; 
+          <a href='https://kartoza.com' target='_blank' style={{ color: "var(--white)"}}>Kartoza</a> */}
+            <div className='sanbi-footer-logo-container'>
+                <img className='footer-logo' data-testid='sanbi-footer-logo'
+                src='/static/images/footer/footer-logo.png'/>
+            </div>
+            <div className='footer-nav' data-testid='footer-navigation'>
+                <a href='/' target='_blank'>HOME</a>
+                <a href='/map' target='_blank'>MAP</a>
+                <a href='/' target='_blank'>DOCUMENTATION</a>
+                <a href='/contact' target='_blank'>CONTACT</a>
+          </div>
+          <div className='vendors-logos-container' data-testid='vendors-logos-container'>
+            <img className='ids-logo' src='/static/images/footer/ids-logo.png' data-testid='ids-logo'/>
+            <img className='kartoza-white-logo' src='/static/images/footer/kartoza-white.png' data-testid='kartoza-logo'/>
+          </div>
+      </div>
+    );
+  }
+
+
+  export default Footer
--- django_project/frontend/src/containers/HomePage/index.tsx
+++ django_project/frontend/src/containers/HomePage/index.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
 import './index.scss';
 import LandingPageBanner from '../../components/LandingPage/LandingPageBanner';
-
+import Footer from '../../components/Footer'
 const isLoggedIn = (window as any).isLoggedIn;
 import LandingPagePopulationOverview from '../../../src/components/LandingPage/LandingPagePopulationOverview'
-
-function Footer() {
-  return (
-    <div className='footer'>
-        Made with <img className='heart' src='/static/images/heart-icon.png'/> by&nbsp; 
-        <a href='https://kartoza.com' target='_blank' style={{ color: "var(--white)"}}>Kartoza</a>
-    </div>
-  );
-}
-
 
 function HomePage() {
   return (
