22010160d31e602679a8588ecc8432579cfe57e5
Andriamiadantsoa <fanevanjanahary@gmail.com>
2023-07-01 09:20:55 +0300

    Add colour picker

--- django_project/species/admin.py
+++ django_project/species/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from species.models import TaxonRank, Taxon, ManagementStatus, OwnedSpecies
+from species.forms import TaxonForm
+
+
+class TaxonAdmin(admin.ModelAdmin):
+    form = TaxonForm
+
 
 admin.site.register(TaxonRank)
-admin.site.register(Taxon)
+admin.site.register(Taxon, TaxonAdmin)
 admin.site.register(ManagementStatus)
 admin.site.register(OwnedSpecies)
 
