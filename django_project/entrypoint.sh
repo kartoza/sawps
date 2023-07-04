4dad985399ddcae7ac4cf834fc8c77581c1be566
Danang <danangmassandy@gmail.com>
2023-07-04 19:01:26 +0100

    move frontend app build to docker file

--- django_project/entrypoint.sh
+++ django_project/entrypoint.sh
@@ -8,12 +8,13 @@ echo "-----------------------------------------------------"
 echo "STARTING DJANGO ENTRYPOINT $(date)"
 echo "-----------------------------------------------------"
 
+# 2023-07-04: move to dockerfile build
 # Run NPM
-cd /home/web/django_project/frontend
-echo "Installing FrontEnd libraries..."
-npm install --legacy-peer-deps
-echo "Building FrontEnd..."
-npm run build
+# cd /home/web/django_project/frontend
+# echo "Installing FrontEnd libraries..."
+# npm install --legacy-peer-deps
+# echo "Building FrontEnd..."
+# npm run build
 
 # Run initialization
 cd /home/web/django_project
