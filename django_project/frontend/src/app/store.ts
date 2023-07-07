67850c6ef1f44170eb1e4f3d4c511291e2f36495
ketankartoza <138113337+ketankartoza@users.noreply.github.com>
2023-07-07 14:03:55 +0530

    Feature filtering panel for platform data (#605)

--- django_project/frontend/src/app/store.ts
+++ django_project/frontend/src/app/store.ts
@@ -2,12 +2,14 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import LayerFilterReducer from '../reducers/LayerFilter';
 import MapStateReducer from '../reducers/MapState';
 import UploadStateReducer from '../reducers/UploadState';
+import SpeciesFilterReducer from '../reducers/SpeciesFilter';
 
 export const store = configureStore({
     reducer: {
       layerFilter: LayerFilterReducer,
       mapState: MapStateReducer,
-      uploadState: UploadStateReducer
+      uploadState: UploadStateReducer,
+      SpeciesFilter:SpeciesFilterReducer
     },
 });
 
--- django_project/species/views.py
+++ django_project/species/views.py
@@ -1,3 +1,18 @@
-# from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
+from .models import Taxon
+from .serializers import TaxonSerializer
 # Create your views here.
+
+
+class TaxonListAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        taxon = Taxon.objects.all()
+        return Response(
+            status=200,
+            data=TaxonSerializer(taxon, many=True).data
+        )
