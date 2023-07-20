cccaa13a294b53ab3965929b522aaf924b1d7442
Tinashe <70011086+DONOTFORGET-BYTE@users.noreply.github.com>
2023-07-20 03:34:07 +0200

    patch: update file #590

--- django_project/stakeholder/admin.py
+++ django_project/stakeholder/admin.py
@@ -17,14 +17,16 @@ admin.site.register(LoginStatus)
 admin.site.register(UserProfile)
 admin.site.register(Organisation)
 
+
 @admin.register(OrganisationUser)
 class OrganisationUserAdmin(admin.ModelAdmin):
     list_display = ("user", "organisation")
 
+
 @admin.register(OrganisationRepresentative)
 class OrganisationRepresentativeAdmin(admin.ModelAdmin):
     list_display = ("user", "organisation")
-    
+
 
 @admin.register(OrganisationInvites)
 class OrganisationInvitesAdmin(admin.ModelAdmin):
@@ -36,6 +38,7 @@ class OrganisationInvitesAdmin(admin.ModelAdmin):
         "assigned_as"
     )
 
+
 @admin.register(Reminders)
 class RemindersAdmin(admin.ModelAdmin):
     list_display = (
@@ -45,6 +48,6 @@ class RemindersAdmin(admin.ModelAdmin):
         "date",
         "status",
         "type",
-        "email_sent"
+        "email_sent",
         "organisation"
     )
