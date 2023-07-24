aea47206d1efe84687dbe917fe774c0ab89e3540
ketan <ketan@kartoza.com>
2023-07-24 12:19:33 +0530

    Remove unused import

--- django_project/frontend/src/containers/MainPage/Metrics/index.tsx
+++ django_project/frontend/src/containers/MainPage/Metrics/index.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { Box } from "@mui/material";
-import ActivityBarChart from "./ActivityBarChart";
 import SpeciesLineChart from "./SpeciesLineChart";
-import ActivityDonutChart from "./ActivityDonutChart";
 import "./index.scss";
-import CategoryBarChart from "./CategoryBarChart";
 
 const Metrics = () => {
     return (
