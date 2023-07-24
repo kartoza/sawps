e99d267e27951ee2b6ba8dd242d7c2789d3c03ab
Andriamiadantsoa <fanevanjanahary@gmail.com>
2023-07-24 23:31:05 +0300

    add mkdocstring in code

--- django_project/sawps/password_validation.py
+++ django_project/sawps/password_validation.py
@@ -3,19 +3,40 @@ from django.core.exceptions import ValidationError
 
 
 class NumberValidator(object):
+    """Checking if password contains a number character.
+
+    """
     def validate(self, password, user=None):
-        if not re.findall('\d', password):
+        """Check password.
+
+        :param
+        password: Input password
+        :type
+        password: str
+        """
+        if not re.findall(r'\d', password):
             raise ValidationError(
                 "The password must contain a numeric character",
                 code='password_no_number',
             )
 
     def get_help_text(self):
-        return "Numeric caracter"
+        return "Numeric character"
 
 
 class UppercaseValidator(object):
+    """Checking if password contains an uppercase letter.
+
+    """
     def validate(self, password, user=None):
+        """Check password.
+
+        :param
+        password: Input password
+        :type
+        password: str
+        """
+
         if not re.findall('[A-Z]', password):
             raise ValidationError(
                 "The password must contain an uppercase letter",
@@ -27,10 +48,20 @@ class UppercaseValidator(object):
 
 
 class SymbolValidator(object):
+    """Checking if password contains a special character.
+
+    """
     def validate(self, password, user=None):
-        if not re.findall('[()[\]{}|\\`~!@#$%^&*_\-+=;:\'",<>./?]', password):
+        """Check password.
+
+        :param
+        password: Input password
+        :type
+        password: str
+        """
+        if not re.findall(r'[()[\]{}|\\`~!@#$%^&*_\-+=;:\'",<>./?]', password):
             raise ValidationError(
-                "The password must contain a special character",
+                "The password must contain a special character.",
                 code='password_no_symbol',
             )
 
