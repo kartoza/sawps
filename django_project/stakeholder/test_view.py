0886df156b2aea6d4a4e0024b7b859c345e0313e
Tinashe <70011086+DONOTFORGET-BYTE@users.noreply.github.com>
2023-07-25 13:02:07 +0200

    patch: add more tests for coverage #590

--- django_project/stakeholder/test_view.py
+++ django_project/stakeholder/test_view.py
@@ -25,7 +25,7 @@ from stakeholder.models import (
 from django.urls import reverse
 from django.contrib.auth.models import User
 from datetime import datetime, timedelta
-from stakeholder.views import RemindersView,NotificationsView
+from stakeholder.views import RemindersView, NotificationsView
 from regulatory_permit.models import DataUsePermission
 import json
 from django.db.models import QuerySet
@@ -931,7 +931,14 @@ class NotificationsViewTest(TestCase):
         self.assertIsNotNone(response)
 
 
-    def test_get_notification(self):
+    @patch('stakeholder.views.get_reminder_or_notification')
+    @patch('stakeholder.views.convert_reminder_dates')
+    @patch('frontend.serializers.stakeholder.ReminderSerializer')
+    def test_get_notification(
+        self,
+        mock_reminder,
+        mock_convert_dates,
+        mock_reminders_serializer):
         url = reverse('notifications', kwargs={'slug': self.user.username})
         data = {
             'action': 'get_notification',
@@ -947,7 +954,14 @@ class NotificationsViewTest(TestCase):
 
         self.assertIsNotNone(response)
 
-    def test_search_notifications(self):
+    @patch('stakeholder.views.search_reminders_or_notifications')
+    @patch('stakeholder.views.convert_reminder_dates')
+    @patch('frontend.serializers.stakeholder.ReminderSerializer')
+    def test_search_notifications(
+        self,
+        mock_reminder,
+        mock_convert_dates,
+        mock_reminders_serializer):
         url = reverse('notifications', kwargs={'slug': self.user.username})
         data = {
             'action': 'search_notifications',
@@ -964,7 +978,14 @@ class NotificationsViewTest(TestCase):
 
         self.assertIsNotNone(response)
 
-    def test_delete_notification(self):
+    @patch('stakeholder.views.delete_reminder_and_notification')
+    @patch('stakeholder.views.convert_reminder_dates')
+    @patch('frontend.serializers.stakeholder.ReminderSerializer')
+    def test_delete_notification(
+        self,
+        mock_reminder,
+        mock_convert_dates,
+        mock_reminders_serializer):
         url = reverse('notifications', kwargs={'slug': self.user.username})
         data = {
             'action': 'delete_notification',
@@ -982,69 +1003,83 @@ class NotificationsViewTest(TestCase):
         self.assertIsNotNone(response)
 
     def test_dispatch_get_notification(self):
+        # Simulate a POST request with action 'get_notification'
         url = reverse('notifications', kwargs={'slug': self.user.username})
         data = {
             'action': 'get_notification',
-            'ids': [json.dumps(self.reminder1.pk)],
-            'csrfmiddlewaretoken': self.client.cookies.get('csrftoken', '')
+            'notifications_page': True,
+            'ids': [self.reminder1.id],
+            'csrfmiddlewaretoken': self.client.cookies.get('csrftoken', ''),
         }
         request = self.factory.post(url, data)
         request.user = self.user
         request.session = {CURRENT_ORGANISATION_ID_KEY: self.organisation}
+        view = NotificationsView.as_view()
+        response = view(request)
 
-        view = NotificationsView()
-        response = view.get_notification(request)
-
-        self.assertIsNotNone(response)
+        self.assertEqual(response.status_code, 200)
 
     def test_dispatch_search_notifications(self):
         url = reverse('notifications', kwargs={'slug': self.user.username})
         data = {
             'action': 'search_notifications',
-            'query': 'Remin',
+            'query': 'i',
             'notifications_page': True,
-            'csrfmiddlewaretoken': '{{ csrf_token }}'
+            'ids': [self.reminder1.id],
+            'csrfmiddlewaretoken': self.client.cookies.get('csrftoken', ''),
         }
         request = self.factory.post(url, data)
         request.user = self.user
         request.session = {CURRENT_ORGANISATION_ID_KEY: self.organisation}
 
-        view = NotificationsView()
-        response = view.search_notifications(request)
+        view = NotificationsView.as_view()
+        response = view(request)
 
-        self.assertIsNotNone(response)
+        self.assertEqual(response.status_code, 200)
 
     def test_dispatch_delete_notification(self):
+        # Simulate a POST request with action 'delete_notification'
         url = reverse('notifications', kwargs={'slug': self.user.username})
         data = {
             'action': 'delete_notification',
             'notifications_page': True,
-            'ids': [json.dumps(self.reminder1.pk)],
-            'csrfmiddlewaretoken': '{{ csrf_token }}'
+            'ids': [self.reminder1.id],
+            'csrfmiddlewaretoken': self.client.cookies.get('csrftoken', ''),
         }
-
         request = self.factory.post(url, data)
         request.user = self.user
         request.session = {CURRENT_ORGANISATION_ID_KEY: self.organisation}
 
-        view = NotificationsView()
-        response = view.delete_notification(request)
+        # Create an instance of the NotificationsView and call the dispatch method
+        view = NotificationsView.as_view()
+        response = view(request)
 
-        self.assertIsNotNone(response)
+        self.assertEqual(response.status_code, 200)
 
-    def test_get_context_data(self):
+    def test_dispatch_default_action(self):
+        # Simulate a POST request with an invalid action
         url = reverse('notifications', kwargs={'slug': self.user.username})
         data = {
-            'action': 'search_notifications',
-            'query': 'Remin',
-            'notifications_page': True,
-            'csrfmiddlewaretoken': '{{ csrf_token }}'
+            'action': 'invalid_action',
+            'ids': [self.reminder1.id],
+            'csrfmiddlewaretoken': self.client.cookies.get('csrftoken', ''),
         }
         request = self.factory.post(url, data)
         request.user = self.user
+
+        view = NotificationsView.as_view()
+        response = view(request)
+
+        self.assertEqual(response.status_code, 405)
+
+    def test_get_context_data(self):
+        # Simulate a GET request to the notifications view
+        url = reverse('notifications', kwargs={'slug': self.user.username})
+        request = self.factory.get(url)
+        request.user = self.user
         request.session = {CURRENT_ORGANISATION_ID_KEY: self.organisation}
 
-        view = NotificationsView()
-        response = view.search_notifications(request)
+        view = NotificationsView.as_view()
+        response = view(request)
 
-        self.assertIsNotNone(response)
+        self.assertIn('notifications', response.context_data)
