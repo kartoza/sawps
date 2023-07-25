0a815a8f9da8f32e1ba47e9de18ad7a849c17f9f
Tinashe <70011086+DONOTFORGET-BYTE@users.noreply.github.com>
2023-07-25 12:10:33 +0200

    patch: add test for coverage #590

--- django_project/stakeholder/test_view.py
+++ django_project/stakeholder/test_view.py
@@ -25,7 +25,7 @@ from stakeholder.models import (
 from django.urls import reverse
 from django.contrib.auth.models import User
 from datetime import datetime, timedelta
-from stakeholder.views import RemindersView
+from stakeholder.views import RemindersView,NotificationsView
 from regulatory_permit.models import DataUsePermission
 import json
 from django.db.models import QuerySet
@@ -877,24 +877,6 @@ class RemindersViewTest(TestCase):
 
         self.assertIsInstance(response, JsonResponse)
 
-from django.test import (
-    TestCase,
-    Client,
-    RequestFactory
-)
-from frontend.utils.organisation import CURRENT_ORGANISATION_ID_KEY
-from stakeholder.models import (
-    Reminders,
-    Organisation,
-    OrganisationUser
-)
-from django.urls import reverse
-from django.contrib.auth.models import User
-from stakeholder.views import NotificationsView
-from regulatory_permit.models import DataUsePermission
-from stakeholder.models import Reminders
-import json
-
 
 class NotificationsViewTest(TestCase):
     def setUp(self):
@@ -999,3 +981,70 @@ class NotificationsViewTest(TestCase):
 
         self.assertIsNotNone(response)
 
+    def test_dispatch_get_notification(self):
+        url = reverse('notifications', kwargs={'slug': self.user.username})
+        data = {
+            'action': 'get_notification',
+            'ids': [json.dumps(self.reminder1.pk)],
+            'csrfmiddlewaretoken': self.client.cookies.get('csrftoken', '')
+        }
+        request = self.factory.post(url, data)
+        request.user = self.user
+        request.session = {CURRENT_ORGANISATION_ID_KEY: self.organisation}
+
+        view = NotificationsView()
+        response = view.get_notification(request)
+
+        self.assertIsNotNone(response)
+
+    def test_dispatch_search_notifications(self):
+        url = reverse('notifications', kwargs={'slug': self.user.username})
+        data = {
+            'action': 'search_notifications',
+            'query': 'Remin',
+            'notifications_page': True,
+            'csrfmiddlewaretoken': '{{ csrf_token }}'
+        }
+        request = self.factory.post(url, data)
+        request.user = self.user
+        request.session = {CURRENT_ORGANISATION_ID_KEY: self.organisation}
+
+        view = NotificationsView()
+        response = view.search_notifications(request)
+
+        self.assertIsNotNone(response)
+
+    def test_dispatch_delete_notification(self):
+        url = reverse('notifications', kwargs={'slug': self.user.username})
+        data = {
+            'action': 'delete_notification',
+            'notifications_page': True,
+            'ids': [json.dumps(self.reminder1.pk)],
+            'csrfmiddlewaretoken': '{{ csrf_token }}'
+        }
+
+        request = self.factory.post(url, data)
+        request.user = self.user
+        request.session = {CURRENT_ORGANISATION_ID_KEY: self.organisation}
+
+        view = NotificationsView()
+        response = view.delete_notification(request)
+
+        self.assertIsNotNone(response)
+
+    def test_get_context_data(self):
+        url = reverse('notifications', kwargs={'slug': self.user.username})
+        data = {
+            'action': 'search_notifications',
+            'query': 'Remin',
+            'notifications_page': True,
+            'csrfmiddlewaretoken': '{{ csrf_token }}'
+        }
+        request = self.factory.post(url, data)
+        request.user = self.user
+        request.session = {CURRENT_ORGANISATION_ID_KEY: self.organisation}
+
+        view = NotificationsView()
+        response = view.search_notifications(request)
+
+        self.assertIsNotNone(response)
