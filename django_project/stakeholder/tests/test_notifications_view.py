9e8e733d284bad1f2198e7a39c87c033f21cc5cc
Tinashe <70011086+DONOTFORGET-BYTE@users.noreply.github.com>
2023-07-25 14:28:13 +0200

    patch: update tests #590

--- django_project/stakeholder/tests/test_notifications_view.py
+++ django_project/stakeholder/tests/test_notifications_view.py
@@ -1,4 +1,60 @@
-def test_get_notifications(self):
+from django.test import (
+    TestCase,
+    Client,
+    RequestFactory
+)
+from frontend.utils.organisation import CURRENT_ORGANISATION_ID_KEY
+from stakeholder.models import (
+    Reminders,
+    Organisation,
+    OrganisationUser
+)
+from django.urls import reverse
+from django.contrib.auth.models import User
+from stakeholder.views import NotificationsView
+from regulatory_permit.models import DataUsePermission
+from stakeholder.models import Reminders
+import json
+from unittest.mock import patch
+
+
+class NotificationsViewTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword123454$',
+            email='email@gamil.com'
+        )
+        self.data_use_permission = DataUsePermission.objects.create(
+            name="test"
+        )
+        self.organisation = Organisation.objects.create(
+            name="test_organisation",
+            data_use_permission = self.data_use_permission
+        )
+        self.organisation_user = OrganisationUser.objects.create(
+            organisation=self.organisation,
+            user=self.user
+        )
+        self.reminder1 = Reminders.objects.create(
+            title='Test Reminder 1',
+            user=self.user,
+            organisation=self.organisation,
+            reminder='Test Reminder Note',
+            email_sent=True,
+            status=Reminders.PASSED
+        )
+        self.reminder2 = Reminders.objects.create(
+            title='Test Reminder 2',
+            user=self.user,
+            organisation=self.organisation,
+            reminder='Test Reminder Note'
+        )
+        self.client = Client()
+
+
+    def test_get_notifications(self):
 
         url = reverse('notifications', kwargs={'slug': self.user.username})
         data = {
