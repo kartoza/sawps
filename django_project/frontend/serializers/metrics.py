c86014d93465b19914e5add834ff639e208f24a0
ketankartoza <138113337+ketankartoza@users.noreply.github.com>
2023-07-27 12:07:30 +0530

    Update django_project/frontend/serializers/metrics.py
    Co-authored-by: Dimas Ciputra <dimas@kartoza.com>

--- django_project/frontend/serializers/metrics.py
+++ django_project/frontend/serializers/metrics.py
@@ -75,7 +75,7 @@ class ActivityMatrixSerializer(serializers.ModelSerializer):
 
     def get_activities(self, obj):
         property = self.context['request'].GET.get('property')
-        property_list = property.split(',') if property else None
+        property_list = property.split(',') if property else []
         owned_species = OwnedSpecies.objects.values(
             "taxon__common_name_varbatim"
         ).filter(taxon=obj)
