afa459db1a04def031894500ea5c136f05045d87
Danang <danangmassandy@gmail.com>
2023-08-10 11:49:39 +0000

    remove multistage for plumber image

--- django_project/frontend/admin.py
+++ django_project/frontend/admin.py
@@ -213,8 +213,6 @@ class StatisticalModelAdmin(admin.ModelAdmin):
     inlines = [StatisticalModelOutputInline]
 
     def response_change(self, request, obj):
-        print('hereeeee')
-        print(request.POST['_download-data-template'])
         if '_download-data-template' in request.POST:
             template_file = absolute_path(
                 'frontend', 'utils', 'data_sample.csv'
