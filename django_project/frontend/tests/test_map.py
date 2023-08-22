9749f5809cf627a6e15b9cf3163566804e9135a2
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-08-22 23:12:51 +0100

    patch: add tests for coverage

--- django_project/frontend/tests/test_map.py
+++ django_project/frontend/tests/test_map.py
@@ -7,9 +7,12 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.sessions.middleware import SessionMiddleware
 from core.settings.utils import absolute_path
+from frontend.utils.map import get_map_template_style
+from stakeholder.models import UserProfile
 from property.factories import PropertyFactory
 from stakeholder.factories import (
-    organisationFactory
+    organisationFactory,
+    userRoleTypeFactory
 )
 from frontend.models.parcels import (
     Erf,
@@ -81,8 +84,48 @@ class TestMapAPIViews(TestCase):
         session_key = None
         request.session = engine.SessionStore(session_key)
         view = MapStyles.as_view()
-        response = view(request)
+        response = view(request) 
         self.assertEqual(response.status_code, 200)
+    
+    def test_map_styles_with_different_role(self):
+        # test with user role as decision maker
+        role = userRoleTypeFactory.create(
+            id=1,
+            name = 'Decision Maker',
+        )
+        UserProfile.objects.create(
+            user=self.user_1,
+            user_role_type_id=role
+        )
+        request = self.factory.get(
+            reverse('map-style')
+        )
+        request.user = self.user_1
+        response = get_map_template_style(request)
+    
+    def test_map_styles_with_theme_value_for_role(self):
+        # test with user role as decision maker
+        role = userRoleTypeFactory.create(
+            id=1,
+            name = 'Decision Maker',
+        )
+        UserProfile.objects.create(
+            user=self.user_1,
+            user_role_type_id=role
+        )
+        request = self.factory.get(
+            reverse('map-style')
+        )
+        request.user = self.user_1
+        response = get_map_template_style(request,1)
+
+    def test_map_styles_with_different_role(self):
+        request = self.factory.get(
+            reverse('map-style')
+        )
+        request.user = self.user_1
+        response = get_map_template_style(request,1)
+        self.assertIsNotNone(response['layers'])
 
     def test_get_properties_map_tile(self):
         kwargs = {
