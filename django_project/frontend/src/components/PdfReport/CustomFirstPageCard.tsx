cbf534ab564790ef6a36d7871f5d95520a65aa7d
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-08-24 10:44:51 +0100

    feat: first page components for pdf #777

---
+++ django_project/frontend/src/components/PdfReport/CustomFirstPageCard.tsx
@@ -0,0 +1,57 @@
+import React, { FC } from 'react';
+import './styles.scss';
+import SpeciesChart from '../LandingPage/LandingPagePopulationOverview/SpeciesChart';
+import Skeleton from '@mui/material/Skeleton';
+
+export interface IColors {
+  line?: string;
+  area?: string;
+}
+
+interface ISpeciesCardProps {
+  pic: string;
+  species_id: number;
+  species_name?: string;
+  growth: string;
+  loss: string;
+  chartColors: IColors;
+  index?: number;
+  population?: string;
+  textColor?: string; // Add the textColor prop
+}
+
+const CustomFirstPageCard: FC<ISpeciesCardProps> = (props) => {
+  const textColor = props.textColor || 'black'; // Default to black if not provided
+
+  return (
+    <div className='species-card-container' data-testid='species-card-container'>
+      <div className='species-card-image-container'>
+        {props.pic ? (
+          <img src={props.pic} className='species-card-image' data-testid='species-card-image' />
+        ) : (
+          <Skeleton variant='circular' className='species-card-no-image' />
+        )}
+        <p className='species-card-text species-name-text' style={{ color: textColor }}>
+          {props.species_name}
+        </p>
+        <hr />
+      </div>
+      <div className='species-card-text-container'>
+        <p className='species-card-text' style={{ color: textColor }} data-testid='species-card-population'>
+          Total Population : {props.population}
+        </p>
+        <p className='species-card-text' style={{ color: textColor }} data-testid='species-card-growth'>
+          Population Growth : {props.growth}
+        </p>
+        <p className='species-card-text' style={{ color: textColor }} data-testid='species-card-loss'>
+          Population Loss : {props.loss}
+        </p>
+      </div>
+      <div className='ChartHolder'>
+        <SpeciesChart species_id={props.species_id} species_name={props.species_name} lineColor={props.chartColors.line} areaFillColor={props.chartColors.area} index={props.index} />
+      </div>
+    </div>
+  );
+};
+
+export default CustomFirstPageCard;
