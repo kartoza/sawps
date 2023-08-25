96f4231bf7f7537d11737d0127f23237fd6473c1
ketan <ketan@kartoza.com>
2023-08-25 12:39:09 +0530

    Added unique year for the annualpopulation year

--- django_project/species/factories.py
+++ django_project/species/factories.py
@@ -6,6 +6,7 @@ from population_data.factories import (
     AnnualPopulationF,
     AnnualPopulationPerActivityFactory,
 )
+from population_data.models import AnnualPopulation
 from species.models import OwnedSpecies, Taxon, TaxonRank, TaxonSurveyMethod
 
 
@@ -58,7 +59,9 @@ class OwnedSpeciesFactory(factory.django.DjangoModelFactory):
 
     @factory.post_generation
     def create_annual_population(self, create, extracted, **kwargs):
-        year = randint(2000, 2022)
+        year = randint(1960, 2000)
+        if len(AnnualPopulation.objects.filter(year=year)) > 0:
+            year = randint(2001, 2023)
         AnnualPopulationF.create(
             year=year,
             owned_species=self,
@@ -75,7 +78,9 @@ class OwnedSpeciesFactory(factory.django.DjangoModelFactory):
 
     @factory.post_generation
     def create_annual_population_per_activity(self, create, *args):
-        year = randint(2000, 2022)
+        year = randint(1960, 2000)
+        if len(AnnualPopulation.objects.filter(year=year)) > 0:
+            year = randint(2001, 2023)
         AnnualPopulationPerActivityFactory.create(
             year=year,
             owned_species=self,
