5ac21bbd447d21dca5793b78a3760a7f4e7f9ae0
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-08-28 01:23:42 +0100

    feat: add tests for national statistics views

---
+++ django_project/frontend/tests/test_national_statistics.py
@@ -0,0 +1,207 @@
+import base64
+import datetime
+from django.urls import reverse
+from rest_framework.test import APITestCase, APIRequestFactory
+from rest_framework import status
+from unittest.mock import patch
+from frontend.utils.organisation import CURRENT_ORGANISATION_ID_KEY
+from frontend.api_views.map import User
+from stakeholder.factories import organisationFactory
+from species.models import Taxon
+from frontend.serializers.national_statistics import (
+    SpeciesListSerializer,
+    NationalStatisticsSerializer
+)
+from django.contrib.auth import get_user_model
+from django.test import Client, RequestFactory
+from django_otp.plugins.otp_totp.models import TOTPDevice
+import json
+from django.test import TestCase
+from django.contrib.sessions.middleware import SessionMiddleware
+from property.models import Property, PropertyType, Province
+from frontend.api_views.national_statistic import (
+    NationalPropertiesView,
+    NationalActivityCountView
+)
+from rest_framework.response import Response
+
+
+class NationalSpeciesViewTest(APITestCase):
+    def setUp(self):
+        self.url = reverse('species_list_national')
+
+    @patch('frontend.api_views.national_statistic.NationalSpeciesView.get_species_list')
+    def test_get_species_list(self, mock_get_species_list):
+        # Create mock Taxon objects
+        taxon1 = Taxon(common_name_varbatim='Species 1')
+        taxon2 = Taxon(common_name_varbatim='Species 2')
+        test_user = get_user_model().objects.create_user(
+            username='testuser', password='testpassword'
+        )
+        device = TOTPDevice(
+            user=test_user,
+            name='device_name'
+        )
+        device.save()
+        client = Client()
+        resp = client.login(username='testuser', password='testpassword')
+
+        self.assertEqual(resp, True)
+
+
+        # Mock the get_species_list method to return the mock Taxon objects
+        mock_get_species_list.return_value = [taxon1, taxon2]
+
+        response = client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+        serializer = SpeciesListSerializer([taxon1, taxon2], many=True)
+        self.assertEqual(response.data, serializer.data)
+
+    @patch('frontend.api_views.national_statistic.NationalSpeciesView.get_species_list')
+    def test_get_species_list_empty(self, mock_get_species_list):
+        # Mock the get_species_list method to return an empty list
+        mock_get_species_list.return_value = []
+        test_user = get_user_model().objects.create_user(
+            username='testuser', password='testpassword'
+        )
+        device = TOTPDevice(
+            user=test_user,
+            name='device_name'
+        )
+        device.save()
+        client = Client()
+        resp = client.login(username='testuser', password='testpassword')
+
+        self.assertEqual(resp,True)
+
+        response = client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 0)
+
+
+
+class NationalStatisticsViewTest(APITestCase):
+    def setUp(self):
+        self.url = reverse('statistics_national')
+        self.test_user = get_user_model().objects.create_user(
+            username='testuser', password='testpassword'
+        )
+        self.device = TOTPDevice(
+            user=self.test_user,
+            name='device_name'
+        )
+        self.device.save()
+        self.client = Client()
+
+    @patch('property.models.Property.objects.filter')
+    @patch('species.models.OwnedSpecies.objects.filter')
+    def test_get_statistics(self, mock_owned_species_filter, mock_property_filter):
+        # Mock Property objects and aggregate result
+        mock_property_filter.return_value.count.return_value = 5
+        mock_property_filter.return_value.aggregate.return_value = {'total_area': 100}
+
+        # Mock OwnedSpecies objects and aggregate result
+        mock_owned_species_filter.return_value.aggregate.return_value = {'total_area_to_species': 50}
+
+        client = self.client
+        client.login(username='testuser', password='testpassword')
+        response = client.get(self.url)
+
+        self.assertIsNotNone(response)
+
+    @patch('property.models.Property.objects.filter')
+    @patch('species.models.OwnedSpecies.objects.filter')
+    def test_get_statistics_empty(self, mock_owned_species_filter, mock_property_filter):
+        # Mock Property objects and aggregate result
+        mock_property_filter.return_value.count.return_value = 0
+
+        # Mock OwnedSpecies objects and aggregate result
+        mock_owned_species_filter.return_value.aggregate.return_value = {'total_area_to_species': 0}
+
+        client = self.client
+        client.login(username='testuser', password='testpassword')
+        response = client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsNotNone(response)
+
+
+
+class NationalPropertiesViewTest(TestCase):
+
+    def setUp(self):
+        self.url = reverse('properties_population_category')
+        self.test_user = get_user_model().objects.create_user(
+            username='testuser', password='testpassword'
+        )
+        self.device = TOTPDevice(
+            user=self.test_user,
+            name='device_name'
+        )
+        self.device.save()
+        self.client = Client()
+
+    def test_get_properties_per_population_category(self):
+        # Create a test organization and properties
+        self.organisation = organisationFactory.create()
+        Province.objects.create(name='Gauteng')
+        organisation_id = self.organisation.pk
+        PropertyType.objects.create(name='national')
+        PropertyType.objects.create(name='private')
+        Property.objects.create(
+            organisation_id=organisation_id,
+            property_type=PropertyType.objects.filter(name='national').first(),
+            created_at=datetime.datetime.now(),
+            created_by=self.test_user,
+            province=Province.objects.filter(name='Gauteng').first()
+        )
+        self.auth_headers = {
+            "HTTP_AUTHORIZATION": "Basic "
+            + base64.b64encode(b"testuser:testpassword").decode("ascii"),
+        }
+
+        session = self.client.session
+        session[CURRENT_ORGANISATION_ID_KEY] = self.organisation.id
+        session.save()
+        url = self.url
+        response = self.client.get(url, **self.auth_headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+# class NationalActivityCountViewTestCase(TestCase):
+
+#     def setUp(self):
+#         # Create test properties, species, activity types, and population data
+#         # Populate the test database with data for testing
+#         self.data = {}
+
+#     def test_get_activity_count(self):
+#         view = NationalActivityCountView()
+#         view.request = APIRequestFactory().get("/")
+#         result = view.get_activity_count()
+
+#         # Check if result is a dictionary
+#         self.assertIsInstance(result, dict)
+
+#         # Perform additional checks on the result dictionary
+#         # For example, check if keys and values are correct
+
+#     def test_get(self):
+#         view = NationalActivityCountView()
+#         view.request = APIRequestFactory().get("/")
+#         response = view.get()
+
+#         # Check if the response status code is 200
+#         self.assertEqual(response.status_code, 200)
+        
+#         # Check if the response is of type Response
+#         self.assertIsInstance(response, Response)
+
+#         # Perform additional checks on the response content
+#         # For example, check JSON structure, keys, values, etc.
+
+
+
