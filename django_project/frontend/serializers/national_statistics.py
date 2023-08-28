c707bba94ddb7e65c74b558995f6943f901bd892
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-08-28 01:43:09 +0100

    patch: add icon #777

--- django_project/frontend/serializers/national_statistics.py
+++ django_project/frontend/serializers/national_statistics.py
@@ -12,6 +12,7 @@ class SpeciesListSerializer(serializers.ModelSerializer):
     """
     species_name = serializers.SerializerMethodField()
     species_colour = serializers.SerializerMethodField()
+    icon = serializers.ImageField(use_url=True) 
     annualpopulation_count = serializers.SerializerMethodField()
     total_population = serializers.SerializerMethodField()
     population_growth = serializers.SerializerMethodField()
@@ -23,6 +24,7 @@ class SpeciesListSerializer(serializers.ModelSerializer):
             "id",
             "species_name",
             "species_colour",
+            "icon",
             "annualpopulation_count",
             "total_population",
             "population_growth",
@@ -42,6 +44,13 @@ class SpeciesListSerializer(serializers.ModelSerializer):
             obj (Taxon): The Taxon instance representing the species.
         """
         return obj.colour
+    
+    def get_species_icon(self, obj: Taxon) -> str:
+        """Get the icon of the species.
+        Params:
+            obj (Taxon): The Taxon instance representing the species.
+        """
+        return obj.icon.url
 
     def get_annualpopulation_count(self, obj: Taxon) -> List[dict]:
         """Get the population count per year for the species.
