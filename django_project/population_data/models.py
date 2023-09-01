fcd4f6834d18d50b1b357585ce0b67bff814698f
Danang <danangmassandy@gmail.com>
2023-09-01 08:39:44 +0000

    add population estimate category/survey method other explanation text

--- django_project/population_data/models.py
+++ django_project/population_data/models.py
@@ -86,6 +86,12 @@ class AnnualPopulation(AnnualPopulationAbstract):
     population_estimate_certainty = models.IntegerField(
         null=True, blank=True
     )
+    population_estimate_category_other = models.TextField(
+        null=True, blank=True
+    )
+    survey_method_other = models.TextField(
+        null=True, blank=True
+    )
 
     def __str__(self):
         return "{} {}".format(
