364f3ba25766640f9428198a68690ccc2751e0b6
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-09-02 16:27:59 +0100

    feat: utility function for fetching current org #925

--- django_project/frontend/utils/organisation.py
+++ django_project/frontend/utils/organisation.py
@@ -1,4 +1,14 @@
+from stakeholder.models import UserProfile
+
 """Organisation utility functions."""
 
 CURRENT_ORGANISATION_ID_KEY = 'current_organisation_id'
 CURRENT_ORGANISATION_KEY = 'current_organisation'
+
+
+def get_current_organisation_id(user):
+    try:
+        user_profile = user.user_profile
+        return user_profile.current_organisation.id
+    except UserProfile.DoesNotExist:
+        return None
