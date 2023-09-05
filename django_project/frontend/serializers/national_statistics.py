4eaa88f619fc1f60851e494c08d764c8e7adda82
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-09-05 07:13:05 +0100

    patch: remove duplicate

--- django_project/frontend/serializers/national_statistics.py
+++ django_project/frontend/serializers/national_statistics.py
@@ -36,9 +36,6 @@ class SpeciesListSerializer(serializers.ModelSerializer):
         """
         return obj.common_name_varbatim
 
-    def get_species_icon(self, obj: Taxon) -> str:
-        return obj.icon.url
-
     def get_species_colour(self, obj: Taxon) -> str:
         """Get the color of the species.
         Params:
