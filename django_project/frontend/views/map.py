b895dfa5d122c1342ba5d0bcc1d487f08d02ec60
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-09-11 14:47:19 +0200

    feat: add redirect to explore #955

--- django_project/frontend/views/map.py
+++ django_project/frontend/views/map.py
@@ -22,6 +22,11 @@ def redirect_to_upload(request):
     redirect_url = f"{reverse('map')}?tab={tab_number}"
     return HttpResponseRedirect(redirect_url)
 
+def redirect_to_explore(request):
+    tab_number = 0
+    redirect_url = f"{reverse('map')}?tab={tab_number}"
+    return HttpResponseRedirect(redirect_url)
+
 
 class MapView(LoginRequiredMixin, RegisteredOrganisationBaseView):
     """
