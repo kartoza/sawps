e513d423b2de2adcca512f589eb7b64b0e07825a
Danang <danangmassandy@gmail.com>
2023-09-11 03:37:18 +0000

    fix add province labels table migration

--- django_project/frontend/migrations/0015_add_province_labels_table.py
+++ django_project/frontend/migrations/0015_add_province_labels_table.py
@@ -10,6 +10,14 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunSQL(
+            """CREATE TABLE IF NOT EXISTS layer.zaf_provinces_small_scale (
+                id int8 NULL,
+                geom public.geometry NULL,
+                adm1_en varchar(50) NULL
+            );""",
+            reverse_sql=migrations.RunSQL.noop
+        ),
+        migrations.RunSQL(
             """create table if not exists layer.province_labels as
             select id as id,
                 st_centroid(st_makevalid(geom)) as geom,
