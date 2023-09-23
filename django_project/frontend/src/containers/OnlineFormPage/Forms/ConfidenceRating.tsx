d5c64d9d6bf24dad7be262ba5a1cadede0d9b3df
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-09-23 20:57:16 +0200

    feat: confidence rating component

---
+++ django_project/frontend/src/containers/OnlineFormPage/Forms/ConfidenceRating.tsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import {
+  Modal,
+  Typography,
+  Slider,
+  Paper,
+  Grid,
+  Button,
+} from '@mui/material';
+
+interface ConfidenceRatingProps {
+  open: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  currentConfidence: number;
+  onConfidenceChange: (newValue: number) => void;
+  modalHeight?: string;
+}
+
+const ConfidenceRating: React.FC<ConfidenceRatingProps> = ({
+  open,
+  onClose,
+  onSubmit,
+  currentConfidence,
+  onConfidenceChange,
+  modalHeight = 'auto',
+}) => {
+  const handleSliderChange = (event: Event, newValue: number | number[]) => {
+    if (typeof newValue === 'number') {
+      onConfidenceChange(newValue);
+    }
+  };
+
+  const handleValueClick = (value: number) => {
+    onConfidenceChange(value);
+  };
+
+  const renderValues = () => {
+    const values = Array.from({ length: 10 }, (_, index) => index + 1);
+
+    return (
+      <Grid container spacing={2} justifyContent="center">
+        {values.map((value) => (
+          <Grid item key={value}>
+            <Paper
+              sx={{
+                padding: 2,
+                textAlign: 'center',
+                backgroundColor:
+                  value === currentConfidence ? '#f0f0f0' : 'inherit',
+                cursor: 'pointer', // Add cursor style
+              }}
+              onClick={() => handleValueClick(value)}
+            >
+              {value}
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    );
+  };
+
+  return (
+    <Modal open={open} onClose={onSubmit}>
+      <Paper
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: '40%',
+          height: modalHeight,
+          p: 4,
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <Typography variant="h6" align="center">
+          Population Estimate Certainty
+        </Typography>
+        <Typography variant="body1" align="left" mt={1}>
+          How confident are you that this estimate is close to the true population size?
+        </Typography>
+        <Typography
+          variant="body2"
+          mt={1}
+          textAlign="center"
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}
+        >
+          {currentConfidence === 1 && 'Very Unconfident'}
+          {currentConfidence === 10 && 'Very Confident'}
+        </Typography>
+        <Slider
+          value={currentConfidence}
+          onChange={handleSliderChange}
+          min={1}
+          max={10}
+          step={1}
+        />
+        {renderValues()}
+        <Button
+          variant="contained"
+          onClick={onSubmit}
+          sx={{ alignSelf: 'flex-end', mt: 2 }}
+        >
+          Submit
+        </Button>
+      </Paper>
+    </Modal>
+  );
+};
+
+export default ConfidenceRating;
