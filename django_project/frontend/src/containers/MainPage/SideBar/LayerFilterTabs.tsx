bb15da491240905bc1f696201f5bf43f65953523
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-09-27 04:01:48 +0200

    patch: fix select property or digitise not working #1116

--- django_project/frontend/src/containers/MainPage/SideBar/LayerFilterTabs.tsx
+++ django_project/frontend/src/containers/MainPage/SideBar/LayerFilterTabs.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Box from "@mui/material/Box";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
@@ -7,30 +7,28 @@ import Layers from './Layers';
 import './index.scss';
 import Filter from './Filter';
 
-
-function LayerFilterTabs(props: any) {
-    const { selectedMainTabIdx } = props
+function LayerFilterTabs() {
     const [selectedTabSideBar, setSelectedTabSideBar] = useState(0)
 
     return (
         <Box className='LeftSideBar'>
-            <Box className={`TabHeaders ${selectedMainTabIdx > 0 ? 'Single' : ''}`}>
+            <Box className='TabHeaders'>
                 <Tabs value={selectedTabSideBar}
                     onChange={(event: React.SyntheticEvent, newValue: number) => {
                         setSelectedTabSideBar(newValue)
                     }} aria-label="Left Side Bar Tabs"
                 >
-                    <Tab key={0} label={'FILTERS'} {...a11yProps(0)} />
-                    {selectedMainTabIdx === 0 && <Tab key={1} label={'LAYERS'} {...a11yProps(1)} /> }
+                    <Tab key={0} label={'LAYERS'} {...a11yProps(0)} />
+                    <Tab key={1} label={'FILTERS'} {...a11yProps(1)} />
                 </Tabs>
             </Box>
             <Box className='TabPanels FlexContainerFill'>
                 <Box className='LeftSideBarContent'>
                     <TabPanel key={0} value={selectedTabSideBar} index={0} noPadding>
-                        <Filter />
+                        <Layers />
                     </TabPanel>
                     <TabPanel key={1} value={selectedTabSideBar} index={1} noPadding>
-                        <Layers />
+                        <Filter />
                     </TabPanel>
                 </Box>
             </Box>
