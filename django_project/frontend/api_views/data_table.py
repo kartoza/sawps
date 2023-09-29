0c4d7b300899535c606f4d506fb2c949f9e3a7e0
ketankartoza <138113337+ketankartoza@users.noreply.github.com>
2023-09-29 15:19:12 +0530

    Organisation issue fix (#1136)
    * Organisation issue fix
    
    * Fix flake8 of main branch after rebase

--- django_project/frontend/api_views/data_table.py
+++ django_project/frontend/api_views/data_table.py
@@ -17,7 +17,6 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from species.models import Taxon
-from stakeholder.models import UserProfile
 
 
 class DataTableAPIView(APIView):
