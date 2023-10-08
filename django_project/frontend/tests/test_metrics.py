352ccc5fef81807f166083c1f319d10181d7e078
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-10-08 17:25:21 +0200

    patch: test total area with species name #993

--- django_project/frontend/tests/test_metrics.py
+++ django_project/frontend/tests/test_metrics.py
@@ -50,6 +50,19 @@ class BaseTestCase(TestCase):
             organisation=self.organisation_1, name="PropertyA"
         )
 
+        self.taxon1 = TaxonFactory.create(
+            taxon_rank=taxon_rank,
+            common_name_varbatim="Cheetah",
+            scientific_name = "test_cheetah"
+        )
+
+        self.owned_species1 = OwnedSpeciesFactory.create(
+            taxon=self.taxon1, 
+            user=self.user, 
+            property=self.property,
+            area_available_to_species=50.0
+        )
+
         self.owned_species = OwnedSpeciesFactory.create_batch(
             5, taxon=self.taxon, user=self.user, property=self.property
         )
@@ -282,6 +295,11 @@ class TotalAreaAvailableToSpeciesTestCase(BaseTestCase):
         """
         url = self.url
         response = self.client.get(url, **self.auth_headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]['area'], 50.0)
+        # test with species name
+        data = {'property': self.owned_species1.id, 'species': "test_cheetah"}
+        response = self.client.get(url, data, **self.auth_headers)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data[0]['area'], 50.0)
 
