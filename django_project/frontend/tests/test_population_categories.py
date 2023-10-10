57ec431fc5ee8619b6efa1b780df0036c633e934
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-10-10 12:51:43 +0200

    patch: increase tests coverage #990

--- django_project/frontend/tests/test_population_categories.py
+++ django_project/frontend/tests/test_population_categories.py
@@ -38,11 +38,10 @@ class TestCalculatePopulationCategories(unittest.TestCase):
         # Test the calculation of population categories
         # Create mock annual population data
         annual_population_data = [
-            {'year': 2020, 'population_total': 100},
-            {'year': 2020, 'population_total': 200},
             {'year': 2021, 'population_total': 100},
             {'year': 2022, 'population_total': 400},
             {'year': 2023, 'population_total': 300},
+            {'year': 2024, 'population_total': 200},
         ]
 
         # Create a mock queryset and set its return value
@@ -50,7 +49,6 @@ class TestCalculatePopulationCategories(unittest.TestCase):
         mock_queryset._result_cache = annual_population_data
         mock_filter.return_value = mock_queryset
 
-        # Rest of your test code
         queryset = [
             Property(id=1),
             Property(id=2),
@@ -58,12 +56,18 @@ class TestCalculatePopulationCategories(unittest.TestCase):
         species_name = "Panthera Leo"
         result = calculate_population_categories(queryset, species_name)
 
-        # confirm it contains data
+        # Confirm it contains data
         self.assertIsNotNone(result)
 
         # Assert that each result object contains a 'year' key
         for key, value in result.items():
             self.assertIn('year', value)
+
+        # Test all aspects of calculate function were covered
+        self.assertTrue(any(item['population_total'] >= 100 and item['population_total'] < 250 for item in annual_population_data))
+        self.assertTrue(any(item['population_total'] >= 250 and item['population_total'] < 400 for item in annual_population_data))
+        self.assertTrue(any(item['population_total'] >= 400 and item['population_total'] < 550 for item in annual_population_data))
+
 
 if __name__ == '__main__':
     unittest.main()
