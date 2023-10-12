721550e1c3bb3a0ac1f4b30dd43ee46848279d8d
Dimas Ciputra <dimas@kartoza.com>
2023-10-12 09:06:56 +0700

    Refactoring user roles (#1114)
    * Add templatetag function to check if user is a manager
    
    * Use groups to filter data tables
    
    * Fix flake8 error
    
    * Update test_data_table
    
    * Fix flake8
    
    * Fix test
    
    * Fix flake8 error
    
    * Add function to get user_roles
    
    * Add constants
    
    * Add api view to get user info
    
    * Add redux query to get user info
    
    * Remove localStorage get user_role
    
    * Update user role in frontend
    
    * Add organisation status to user-info api
    
    * Fix tests

--- django_project/frontend/tests/test_templatetags.py
+++ django_project/frontend/tests/test_templatetags.py
@@ -1,6 +1,13 @@
 from django.conf import settings
 from django.template import Context, Template
 from django.test import TestCase
+from django.contrib.auth.models import User
+
+from stakeholder.factories import (
+    organisationFactory
+)
+from stakeholder.models import OrganisationInvites, MANAGER
+from frontend.templatetags.custom_tags import is_organisation_manager
 
 
 class SentryDsnTagTestCase(TestCase):
@@ -19,3 +26,47 @@ class SentryDsnTagTestCase(TestCase):
         delattr(settings, "SENTRY_DSN")
         rendered = self.render_template("{% load custom_tags %}{% sentry_dsn %}")
         self.assertEqual(rendered, "")
+
+
+class IsOrganisationManagerTest(TestCase):
+
+    def setUp(self):
+        # Creating test user
+        self.user = User.objects.create_user(
+            username='test',
+            email='testuser@example.com',
+            password='testpass123'
+        )
+
+        # Creating test organisation
+        self.organisation = organisationFactory.create(
+            name='Test Organisation'
+        )
+
+    def test_non_existent_user(self):
+        self.assertFalse(is_organisation_manager(999, self.organisation.id))
+
+    def test_non_existent_organisation(self):
+        self.assertFalse(is_organisation_manager(self.user.id, 999))
+
+    def test_superuser_is_manager(self):
+        # Making user superuser
+        self.user.is_superuser = True
+        self.user.save()
+
+        self.assertTrue(is_organisation_manager(self.user.id, self.organisation.id))
+
+    def test_normal_user_is_not_manager(self):
+        self.assertFalse(is_organisation_manager(self.user.id, self.organisation.id))
+
+    def test_user_invited_as_manager(self):
+        # Adding user to organisation invites
+        OrganisationInvites.objects.create(
+            email=self.user.email,
+            organisation=self.organisation,
+            assigned_as=MANAGER
+        )
+
+        self.assertTrue(
+            is_organisation_manager(
+                self.user.id, self.organisation.id))
