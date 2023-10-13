2f069c94ec40d6ac1f7108d0af71ebadf6988155
Danang <danangmassandy@gmail.com>
2023-10-13 16:57:20 +0000

    add index for province name

--- django_project/property/models.py
+++ django_project/property/models.py
@@ -17,7 +17,7 @@ class PropertyType(models.Model):
 
 
 class Province(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
