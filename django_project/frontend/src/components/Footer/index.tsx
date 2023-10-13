64dc70886065c52f9634e7ae4dc2e4158ba6149f
Zakki <age.zakki@gmail.com>
2023-10-13 20:30:18 +0700

    Change map footer to explore

--- django_project/frontend/src/components/Footer/index.tsx
+++ django_project/frontend/src/components/Footer/index.tsx
@@ -15,7 +15,7 @@ const Footer:FC<IFooter>=()=>{
             </div>
             <div className='footer-nav' data-testid='footer-navigation'>
                 <a href='/' target='_blank'>HOME</a>
-                <a href='/map' target='_blank'>MAP</a>
+                <a href='/map' target='_blank'>EXPLORE</a>
                 <a href='/' target='_blank'>DOCUMENTATION</a>
                 <a href='/contact' target='_blank'>CONTACT</a>
           </div>
