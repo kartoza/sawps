8c6202972baaf37af4235be1d1bbb09ffc1acee1
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-10-17 03:01:46 +0200

    patch: improve render condition #1231

--- django_project/frontend/src/containers/MainPage/Metrics/DensityBarChart.tsx
+++ django_project/frontend/src/containers/MainPage/Metrics/DensityBarChart.tsx
@@ -31,7 +31,11 @@ const DensityBarChart = (props: any) => {
                 .then((response) => {
                     setLoading(false);
                     if (response.data) {
-                        const filteredData = response.data.filter((item: any) => item.density.density !== null);
+                        const filteredData = response.data.filter((item: any) => (
+                            item.density &&
+                            Array.isArray(item.density.density) &&
+                            item.density.density.length > 0
+                        ));
                         if(filteredData.length > 0){
                             onEmptyDatasets(true)
                         }else onEmptyDatasets(false)
