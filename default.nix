6262254c87e058cc891155d31ed0fd2d88c38af9
Tim Sutton <tim@kartoza.com>
2023-10-26 22:01:00 +0100

    make rstudio work

--- default.nix
+++ default.nix
@@ -2,30 +2,12 @@ with import <nixpkgs> { };
 
 let
   pkgs = import <nixpkgs> {};
+  RStudio-with-my-packages = rstudioWrapper.override{
+      packages = with rPackages; [ tidyverse snakecase ]; };
 in pkgs.mkShell rec {
   name = "R";
   buildInputs = [
-    rstudio
-    rPackages.tidyverse
-    # The rest of these should probably not
-    # be needed since tidyverse would pull them in
-    # anyway
-    curl
-    fontconfig
-    fribidi
-    harfbuzz
-    libtiff
-    pkg-config
-    rPackages.googledrive
-    rPackages.googlesheets4
-    rPackages.httr
-    rPackages.openssl
-    rPackages.ragg
-    rPackages.rvest
-    rPackages.systemfonts
-    rPackages.xml2
-    rstudio
-    xml2
+    RStudio-with-my-packages
   ];
 
 }
