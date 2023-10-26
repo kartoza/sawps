a0abe35a1158d022f3dc8c0ecd02fb09f36fb86e
ketankartoza <138113337+ketankartoza@users.noreply.github.com>
2023-10-26 07:09:16 +0530

    UI changes for the about page (#1406)
    * UI changes for the about page
    
    * Fix playwright tests
    
    * Update test

--- django_project/frontend/e2e/about.spec.ts
+++ django_project/frontend/e2e/about.spec.ts
@@ -4,7 +4,7 @@ let url = '/';
 
 
 test.describe('about page', () => {
-    
+
     //test.use({ storageState: ".auth/adminAuth.json" });
 
     test('page elements', async ({page}) => {
@@ -25,21 +25,21 @@ test.describe('about page', () => {
 
         await page.locator('.col-12').first().isVisible();
 
-        await page.locator('div').filter({ hasText: 'A centralised wildlife population and trade monitoring system for assessing popu' }).nth(3).click();
+        await page.locator('div').filter({ hasText: 'SAWPS is a centralised wildlife population and trade monitoring system for assessing popu' }).nth(3).click();
 
         await page.frameLocator('[data-testid="about-page-video-frame"]').getByLabel('Play', { exact: true}).isVisible();
-        
+
         await page.frameLocator('[data-testid="about-page-video-frame"]').locator('video').click();
 
         await page.getByRole('heading', { name: 'Secure species data storage' }).isVisible();
 
-        await page.getByRole('heading', { name: 'automated state report' }).isVisible();
+        await page.getByRole('heading', { name: 'automated reporting' }).isVisible();
 
         await page.getByRole('heading', { name: 'Data analysis and visualisation' }).isVisible();
 
         await page.getByRole('heading', { name: 'Support National Decision making' }).isVisible();
 
-        await page.getByText('The Wildlife Population system').click();
+        await page.locator('div.about-content-title:has-text("SOUTH AFRICAN WILDLIFE POPULATION SYSTEM (SAWPS)")').click();
 
         const finalURL = page.url();
 
