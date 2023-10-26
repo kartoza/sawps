0a91734159ebe1229f08e32bca995b3c79282feb
Tim Sutton <tim@kartoza.com>
2023-10-26 22:01:46 +0100

    make rstudio work

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 with import <nixpkgs> { };
-
+# See https://discourse.nixos.org/t/how-to-install-rstudio-wrapper-packages-declaratively/30269/7
 let
   pkgs = import <nixpkgs> {};
   RStudio-with-my-packages = rstudioWrapper.override{
