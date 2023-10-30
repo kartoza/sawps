3d10f3b773d1b035734cafd970109c5c9cde1089
Zulfikar Akbar Muzakki <age.zakki@gmail.com>
2023-10-30 10:16:33 +0700

    Feature/1397: filter backend (#1468)
    * Add test
    
    * Add migrations
    
    * Handle province report based on organisation
    
    * Fix failed test
    
    * Update test
    
    * Update test
    
    ---------
    
    Co-authored-by: Dimas Ciputra <dimas@kartoza.com>

--- django_project/frontend/src/containers/MainPage/SideBar/Filter.tsx
+++ django_project/frontend/src/containers/MainPage/SideBar/Filter.tsx
@@ -119,27 +119,17 @@ function Filter(props: any) {
             {
                 id: "Species report",
                 name: "Species report"
-            },
+            }
         ]
 
-        const provinceReportRoles = [
-            'National data scientist',
-            'Regional data scientist',
-            'Organisation member',
-            'Organisation manager'
-        ]
-        if (provinceReportRoles.some(roleExists)) {
+        if (userInfoData.user_permissions.includes("Can view province report")) {
             informationList.push({
                 id: "Province report",
                 name: "Province report"
             })
         }
 
-        const samplingReportRoles = [
-            'National data consumer',
-            'National data scientist'
-        ]
-        if (samplingReportRoles.some(roleExists)) {
+        if (userInfoData.user_permissions.includes("Can view sampling report")) {
             informationList.push({
                 id: "Sampling report",
                 name: "Sampling report"
