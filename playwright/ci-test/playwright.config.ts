0cbcbfafee10cd05bd87659d48c788090f9ae337
Jeff Osundwa <jeff@kartoza.com>
2023-10-30 23:55:49 +0300

    🚚 Moved playwright ci-tests

--- playwright/ci-test/playwright.config.ts
+++ playwright/ci-test/playwright.config.ts
@@ -4,13 +4,13 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-require('dotenv').config();
+//require('dotenv').config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './e2e',
+  testDir: './tests',
   /* Run tests in files in parallel */
   timeout: 30 * 1000,
   fullyParallel: true,
