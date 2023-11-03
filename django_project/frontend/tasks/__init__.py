eeeb6136d8ca76538ad8f4271e0be63e7983aac8
Danang <danangmassandy@gmail.com>
2023-11-04 03:54:48 +0700

    add task to patch province in properties

--- django_project/frontend/tasks/__init__.py
+++ django_project/frontend/tasks/__init__.py
@@ -2,3 +2,4 @@ from .parcel import *  # noqa
 from .generate_vector_tile import *  # noqa
 from .start_plumber import *  # noqa
 from .map_session import *  # noqa
+from .patch_province import *  # noqa
