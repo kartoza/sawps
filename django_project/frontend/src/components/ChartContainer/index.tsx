890cb9c7115e35576dce58170d95dfe28abecaab
Dimas Ciputra <dimas@kartoza.com>
2023-11-03 12:41:45 +0700

    Add download button in chart container (#1514)

--- django_project/frontend/src/components/ChartContainer/index.tsx
+++ django_project/frontend/src/components/ChartContainer/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {useRef} from "react";
 import {Box, Typography} from "@mui/material";
+import html2canvas from "html2canvas";
 import './style.scss';
 
 
@@ -10,10 +11,29 @@ interface ChartContainerInterface {
 }
 
 export default function ChartContainer(props: ChartContainerInterface) {
+    const chartRef = useRef(null);
+
+    const downloadChart = () => {
+        const chartElement = chartRef.current;
+        if (chartElement) {
+            html2canvas(chartElement).then(canvas => {
+                const link = document.createElement('a');
+                link.href = canvas.toDataURL('image/png');
+                link.download = `${props.title}.png`;
+                link.click();
+            });
+        }
+    }
+
     return (
         <Box className={'ChartContainerBox'}>
-            <Typography className={'TextBox'}>{props.title}</Typography>
-            {props.chart}
+            <Box ref={chartRef}>
+                <Typography className={'TextBox'}>{props.title}</Typography>
+                {props.chart}
+            </Box>
+            <Box className={'DownloadChartContainer'}>
+                <img src={'/static/images/download-chart-icon.svg'} width={25} onClick={downloadChart}/>
+            </Box>
         </Box>
     )
 }
