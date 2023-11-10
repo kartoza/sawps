60caa16cd197f3a20b9b00da4025b2418156cf6f
Jeff Osundwa <jeff@kartoza.com>
2023-11-10 11:46:23 +0300

    Updated tests

---
+++ playwright/ci-test/tests/map.spec.ts
@@ -0,0 +1,47 @@
+import { test, expect } from '@playwright/test';
+
+let url = '/';
+
+test.use({
+  storageState: 'auth.json'
+});
+
+test('test', async ({ page }) => {
+
+  await page.goto(url);
+
+  const initialURL = page.url();
+
+  await page.getByRole('button', { name: 'Explore' }).click();
+
+  await page.waitForURL('**/map/');
+
+  await page.getByLabel('Map').click({
+    position: {
+      x: 618,
+      y: 284
+    }
+  });
+  
+  await page.getByLabel('Toggle Dark Mode').click();
+
+  await page.getByLabel('Toggle Light Mode').click();
+
+  await page.getByLabel('Zoom in').click();
+
+  await page.getByLabel('Map').click({
+    position: {
+      x: 297,
+      y: 351
+    }
+  });
+
+  await page.getByLabel('Zoom out').click();
+  
+  await page.getByLabel('Reset bearing to north').click();
+
+  const finalURL = page.url();
+
+  expect(finalURL).not.toBe(initialURL);
+  
+});
---
+++ playwright/ci-test/tests/upload-file.spec.ts
@@ -0,0 +1,58 @@
+import { test, expect } from '@playwright/test';
+
+let url = '/';
+
+test.use({
+  storageState: 'auth.json'
+});
+
+test('test', async ({ page }) => {
+  
+  await page.goto(url);
+
+  const initialURL = page.url();
+
+  await page.getByRole('link', { name: 'UPLOAD DATA' }).click();
+
+  await page.getByRole('button', { name: 'CREATE A NEW PROPERTY' }).click();
+  
+  await page.locator('#input_propertyname').fill('admin-projects');
+
+  await page.getByRole('row', { name: 'Open/Closed System ​' }).getByLabel('​').click();
+  
+  await page.getByRole('option', { name: 'Closed' }).click();
+
+  await page.getByRole('row', { name: 'Property Type ​' }).getByLabel('​').click();
+
+  await page.getByRole('option', { name: 'Provincial' }).click();
+
+  await page.getByRole('button', { name: 'SAVE PROPERTY INFORMATION' }).click();
+
+  const upload = page.getByRole('button', { name: 'UPLOAD' });
+
+  await upload.click();
+
+  const uploadPromise = page.getByRole('button', { name: 'Upload' });
+
+  await expect(uploadPromise).toBeVisible();
+
+  await page.getByText('Uploaded Files').isVisible();
+
+  await expect(page.getByRole('heading', { name: 'Supported formats: zip, json, geojson, gpkg, kml (CRS 4326)' })).toBeVisible();
+
+  const fileChooserPromise = page.waitForEvent('filechooser');
+
+  await page.getByText('Browse').click();
+
+  const fileChooser = await fileChooserPromise;
+
+  await fileChooser.setFiles('tests/fixtures/parcel.geojson');
+
+  await page.getByRole('button', { name: 'UPLOAD FILES' }).click();
+  
+  await page.getByRole('button', { name: 'SAVE BOUNDARY' }).click();
+
+  const finalURL = page.url();
+
+  expect(finalURL).not.toBe(initialURL);
+});
