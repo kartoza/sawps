bce32a360a8e8a29fdfaf808bded701818fe61be
Danang <danangmassandy@gmail.com>
2023-11-17 17:32:42 +0700

    add download to growth chart

--- django_project/frontend/src/containers/MainPage/Trends/GroupedGrowthChart.tsx
+++ django_project/frontend/src/containers/MainPage/Trends/GroupedGrowthChart.tsx
@@ -1,6 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React, {useRef} from 'react';
 import GrowthChart, {GrowthDataItem} from './GrowthChart';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Tooltip } from '@mui/material';
+import html2canvas from "html2canvas";
 import './index.scss';
 
 
@@ -10,18 +11,39 @@ interface GroupedGrowthChartInterface {
     chartId: string;
 }
 
+const INFO_CHART_ICON = '/static/images/information-chart-icon.svg'
+const DOWNLOAD_CHART_ICON = '/static/images/download-chart-icon.svg'
+
 const GroupedGrowthChart = (props: GroupedGrowthChartInterface) => {
+    const chartRef = useRef(null)
+    const downloadChart = () => {
+        const chartElement = chartRef.current;
+        if (chartElement) {
+            html2canvas(chartElement).then(canvas => {
+                const link = document.createElement('a');
+                link.href = canvas.toDataURL('image/png');
+                link.download = `${props.title}.png`;
+                link.click();
+            });
+        }
+    }
     return (
         <Box className={'GroupedGrowthChartContainerBox'}>
-            <Box className={'ChartTitleBox'}>
-                <Typography className={'ChartTitle'}>{props.title}</Typography>
-            </Box>
-            <Box className={'ChartContainers'}>
+            <Box className={'ChartContainers'} ref={chartRef}>
+                <Box className={'ChartTitleBox'}>
+                    <Typography className={'ChartTitle'}>{props.title}</Typography>
+                </Box>
                 <Box className={'ChartContainer'}>
                     <Box className={'ChartBox'}>
                         <GrowthChart data={props.data} chartId={props.chartId} />
                     </Box>
                 </Box>
+            </Box>
+            <Box className={'DownloadChartContainer'}>
+                <Tooltip title={props.title} arrow placement={'left'}>
+                    <img src={INFO_CHART_ICON} width={25}/>
+                </Tooltip>
+                <img src={DOWNLOAD_CHART_ICON} width={25} onClick={downloadChart}/>
             </Box>
         </Box>        
     )
