158cd068c7187f57e947ed98a43048d7e3767655
Zakki <age.zakki@gmail.com>
2023-11-27 11:31:50 +0700

    Fix lint

--- django_project/stakeholder/models.py
+++ django_project/stakeholder/models.py
@@ -1,15 +1,14 @@
-from django.conf import settings
 from django.contrib.auth.models import User, Group
 from django.db import models
 from django.db.models.signals import post_save, post_delete, pre_save
 from django.dispatch import receiver
 from django.utils import timezone
+
+from property.models import Province
 from stakeholder.utils import (
     add_user_to_organisation_group,
     remove_organisation_user_from_group
 )
-
-from property.models import Province
 
 MEMBER = 'Member'
 MANAGER = 'Manager'
