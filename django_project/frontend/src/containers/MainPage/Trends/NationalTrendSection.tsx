3426821f6a000e0b81e9e858c20edeebd1c4d38e
Danang <danangmassandy@gmail.com>
2023-12-01 13:17:36 +0700

    update message when no trend data

--- django_project/frontend/src/containers/MainPage/Trends/NationalTrendSection.tsx
+++ django_project/frontend/src/containers/MainPage/Trends/NationalTrendSection.tsx
@@ -81,7 +81,7 @@ const NationalTrendSection = (props: NationalTrendSectionInterface) => {
                     <Divider />
                 </Grid>
                 <Grid item>
-                    <Grid container flexDirection={'row'} spacing={1}>
+                    {(loadingTrendData || loadingGrowthData || populationTrendData.length > 0) ? <Grid container flexDirection={'row'} spacing={1}>
                         <Grid item sm={12} md={12} lg={5}>
                             <Grid container flexDirection={'column'} className='national-trend-left-side'>
                                 <Grid item>
@@ -116,6 +116,11 @@ const NationalTrendSection = (props: NationalTrendSectionInterface) => {
                             : <Loading containerStyle={{minHeight: 160}}/>}
                         </Grid>
                     </Grid>
+                    : null}
+                    {(!loadingTrendData && !loadingGrowthData && populationTrendData.length === 0) ? <Box className='SectionEmpty'>
+                        {'Insufficient amount of data for this species to generate trend charts.'}
+                    </Box>
+                    :null}
                 </Grid>
             </Grid>
         </Box>
