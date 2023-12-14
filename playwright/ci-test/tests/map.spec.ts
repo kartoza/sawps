7d30365ddcccf167fd2a2800f0dd020c37f471f9
Osundwa Jeff <jeff@kartoza.com>
2023-12-14 04:25:45 +0300

    Updates  on ci-tests (#1798)
    * Updates  on ci-tests
    
    * Update playwright config
    
    * Updated tests
    
    * Minor update on  playright config
    
    * Test fix due to an update on UI

--- playwright/ci-test/tests/map.spec.ts
+++ playwright/ci-test/tests/map.spec.ts
@@ -21,7 +21,13 @@ test('test', async ({ page }) => {
   await expect(page.locator(map)).toBeVisible();
 
   await page.waitForLoadState('domcontentloaded');
+
+  await expect(page.locator('div').filter({ hasText: /^Activity$/ }).first()).toBeVisible();
+
+  await page.locator('nav').filter({ hasText: 'Select' }).last().getByLabel('Open').click();
+
   await expect(page.getByLabel('Map')).toBeVisible();
+  
   await page.getByLabel('Map').click({
     position: {
       x: 458,
@@ -29,7 +35,15 @@ test('test', async ({ page }) => {
     }
   });
   await page.getByLabel('Zoom in').click();
+
   await page.getByLabel('Zoom out').click();
+
+  // 3D view on
+  await page.getByLabel('Toggle 3D view').click();
+
+  // 3D view off
+  await page.getByLabel('Toggle 3D view').click();
+
   await page.getByLabel('Map').click({
     position: {
       x: 495,
@@ -41,6 +55,30 @@ test('test', async ({ page }) => {
 
   await page.waitForURL('**/reports');
 
+  await expect(page.getByText('Report Type')).toBeVisible();
+
+  await page.locator('nav').filter({ hasText: 'Report selected' }).getByLabel('Open').click();
+
+  await page.locator('label').filter({ hasText: 'Select All' }).getByTestId('CheckBoxOutlineBlankIcon').click();
+
+  await page.locator('label').filter({ hasText: 'Select All' }).locator('path').click();
+
+  await page.getByRole('button', { name: 'Close' }).click();
+
+  await expect(page.getByText('Species', { exact: true })).toBeVisible();
+
+  await expect(page.locator('div').filter({ hasText: /^Organisation$/ })).toBeVisible();
+
+  await expect(page.getByPlaceholder('Select').nth(1)).toBeEmpty();
+
+  await page.getByPlaceholder('Select').nth(1).click();
+
+  await expect(page.locator('div').filter({ hasText: /^Property$/ })).toBeVisible();
+
+  await expect(page.getByText('Year')).toBeVisible();
+
+  await expect(page.locator('div').filter({ hasText: /^Activity$/ }).first()).toBeVisible();
+
   await page.getByRole('tab', { name: 'CHARTS' }).click();
 
   await page.waitForURL('**/charts');
@@ -48,6 +86,14 @@ test('test', async ({ page }) => {
   await page.getByRole('tab', { name: 'TRENDS' }).click();
 
   await page.waitForURL('**/trends');
+
+  await expect(page.locator('div').filter({ hasText: /^Species$/ })).toBeVisible();
+
+  await expect(page.locator('div').filter({ hasText: /^Organisation$/ })).toBeVisible();
+
+  await expect(page.locator('div').filter({ hasText: /^Province$/ })).toBeVisible();
+
+  await expect(page.locator('div').filter({ hasText: /^Property$/ })).toBeVisible();
 
   const finalURL = page.url();
 
