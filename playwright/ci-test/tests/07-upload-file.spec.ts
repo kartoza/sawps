b63e4d6f41603b6586d2792872c79ddf262ccad7
Osundwa Jeff <jeff@kartoza.com>
2024-01-08 13:53:07 +0300

    Updates on testing (#1860)
    * Updated playwright testing scripts
    
    * Testing restore layer
    
    * fix container name
    
    * test if visible
    
    * test restore db on CI
    
    * staging tests
    
    * minor fix
    
    * minor fix on pg_restore
    
    * More fixtures for CI
    
    * updated zip fixtures
    
    * test max timeout
    
    * minor changes
    
    * add volumes to worker in docker compose test
    
    * update compact db dump file
    
    * fix ci test
    
    * tests for online form
    
    * fix for failing test
    
    * fix
    
    * fix failed test
    
    * fix: test
    
    * fix test: area section
    
    * fix test: on reports
    
    * fix failing test
    
    * fix tests on charts
    
    * fix scroll into view
    
    * fix scroll into view; undo previous commit
    
    * fix failng test on charts
    
    * added year on form
    
    * modified order of execution
    
    * revert value
    
    ---------
    
    Co-authored-by: Danang <danangmassandy@gmail.com>

---
+++ playwright/ci-test/tests/07-upload-file.spec.ts
@@ -0,0 +1,78 @@
+import { test, expect } from '@playwright/test';
+
+let url = '/';
+
+test.use({
+  storageState: 'auth.json'
+});
+
+test('upload geojson', async ({ page }) => {
+
+  //test.setTimeout(360000)
+  
+  await page.goto(url);
+
+  const initialURL = page.url();
+
+  await page.getByRole('link', { name: 'UPLOAD DATA' }).click();
+
+  await page.getByRole('button', { name: 'CREATE A NEW PROPERTY' }).click();
+  
+  await page.locator('#input_propertyname').fill('admin-property');
+
+  await page.getByRole('row', { name: 'Open/Closed System ​' }).getByLabel('​').click();
+  
+  await page.getByRole('option', { name: 'Closed' }).click();
+
+  await page.getByRole('row', { name: 'Property Type ​' }).getByLabel('​').click();
+
+  await page.getByRole('option', { name: 'Provincial' }).click();
+
+  await page.getByRole('button', { name: 'SAVE PROPERTY INFORMATION' }).click();
+
+  await page.getByRole('button', { name: 'Create Property Boundary'}).isVisible();
+
+  const upload = page.getByRole('button', { name: 'UPLOAD' });
+
+  await upload.click();
+
+  const uploadPromise = page.getByRole('button', { name: 'Upload' });
+
+  await expect(uploadPromise).toBeVisible();
+
+  await page.getByText('Uploaded Files').isVisible();
+
+  await expect(page.getByRole('heading', { name: 'Supported formats: zip, json, geojson, gpkg, kml (CRS 4326)' })).toBeVisible();
+
+  const fileChooserPromise = page.waitForEvent('filechooser');
+
+  await page.getByText('Browse').click();
+
+  const fileChooser = await fileChooserPromise;
+
+  await fileChooser.setFiles('tests/fixtures/plot1.gpkg');
+
+  await expect(page.getByText('plot1.gpkg')).toBeVisible();
+
+  await page.getByRole('button', { name: 'UPLOAD FILES' }).click();
+
+  await page.waitForLoadState('domcontentloaded');
+
+  //await page.getByRole('button', { name: 'PROCESSING FILES...' }).waitFor({state: 'detached'});
+
+  await expect(uploadPromise).toBeHidden();
+  
+  await page.waitForLoadState('domcontentloaded');
+
+  const saveBoundary = page.getByRole('button', { name: 'SAVE BOUNDARY' });
+
+  await expect(saveBoundary).toBeEnabled({timeout: 20000});
+
+  await saveBoundary.click();
+
+  await expect(page.getByText('Upload Species Population Data')).toBeVisible({timeout: 10000});
+ 
+  const finalURL = page.url();
+
+  expect(finalURL).not.toBe(initialURL);
+});
