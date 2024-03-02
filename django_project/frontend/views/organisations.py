1b6c683e2b9da7f325da0856bf0a43f3db3a2e0e
Dimas <dimas.ciputra@gmail.com>
2024-03-02 20:31:22 +0000

    Email the admin about any changes in the organization's data use permissions.

--- django_project/frontend/views/organisations.py
+++ django_project/frontend/views/organisations.py
@@ -1,3 +1,9 @@
+from django.conf import settings
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
+from django.template.loader import render_to_string
+
+from core.settings.contrib import SUPPORT_EMAIL
 from .base_view import RegisteredOrganisationBaseView
 from django.views.generic import TemplateView
 from django.shortcuts import get_object_or_404
@@ -22,6 +28,8 @@ def save_permissions(request, organisation_id):
             'hosting_data_sanbi_other'
         )
 
+        use_of_data_changes = []
+
         only_sanbi = only_sanbi_str.lower() == 'true'
         hosting_data_sanbi = (
             hosting_data_sanbi_str.lower() == 'true'
@@ -35,12 +43,42 @@ def save_permissions(request, organisation_id):
                 pk=organisation_id
             )
 
+            if organisation.use_of_data_by_sanbi_only != only_sanbi:
+                use_of_data_changes.append(
+                    f'Use of data by SANBI only changed from '
+                    f'{organisation.use_of_data_by_sanbi_only} '
+                    f'to {only_sanbi}.'
+                )
             organisation.use_of_data_by_sanbi_only = (
                 only_sanbi
             )
+
+            if (
+                organisation.hosting_through_sanbi_platforms !=
+                hosting_data_sanbi
+            ):
+                use_of_data_changes.append(
+                    f'Hosting / serving the data through SANBI '
+                    f'platforms and portals changed from '
+                    f'{organisation.hosting_through_sanbi_platforms} '
+                    f'to {hosting_data_sanbi}.'
+                )
             organisation.hosting_through_sanbi_platforms = (
                 hosting_data_sanbi
             )
+
+            if (
+                organisation.allowing_sanbi_to_expose_data !=
+                hosting_data_sanbi_other
+            ):
+                use_of_data_changes.append(
+                    f'Hosting through SANBI and allowing SANBI '
+                    f'to publish the Data '
+                    f'to other portals, as well as to actively '
+                    f'curate the data changed from '
+                    f'{organisation.allowing_sanbi_to_expose_data} '
+                    f'to {hosting_data_sanbi_other}.'
+                )
             organisation.allowing_sanbi_to_expose_data = (
                 hosting_data_sanbi_other
             )
@@ -51,6 +89,31 @@ def save_permissions(request, organisation_id):
                 request, 'Your changes have been saved.',
                 extra_tags='notification'
             )
+
+            if len(use_of_data_changes) > 0:
+                changes_list_html = "".join(
+                    [f"<li>{change}</li>" for change in use_of_data_changes])
+                subject = (
+                    f'SAWPS Organisation - '
+                    f'Changes to {organisation.name}\'s Permissions Settings'
+                )
+                message = render_to_string(
+                    'emails/organisation_permission_changed.html',
+                    {
+                        'organisation': organisation.name,
+                        'support_email': SUPPORT_EMAIL,
+                        'changes_list': changes_list_html
+                    }
+                )
+                send_mail(
+                    subject,
+                    None,
+                    settings.SERVER_EMAIL,
+                    list(User.objects.filter(
+                        is_superuser=True
+                    ).values_list('email', flat=True)),
+                    html_message=message
+                )
 
             return JsonResponse(
                 {'message': 'Permissions saved successfully'}
