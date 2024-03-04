fb0cd156111166a6a8b452c17c81835594e92b74
Danang <danangmassandy@gmail.com>
2024-03-04 05:50:12 +0000

    create site preferences model

---
+++ django_project/core/models/preferences.py
@@ -0,0 +1,31 @@
+"""Model for Website Preferences."""
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from core.models.singleton import SingletonModel
+
+
+class SitePreferences(SingletonModel):
+    """Preference settings specifically for website.
+
+    Preference contains
+    - site_title
+    - property_overlaps_threshold
+    """
+
+    site_title = models.CharField(
+        max_length=512,
+        default='SANBI'
+    )
+
+    # -----------------------------------------------
+    # Property Overlaps Threshold for checking overlaps between properties
+    # -----------------------------------------------
+    property_overlaps_threshold = models.FloatField(
+        null=True,
+        blank=True,
+        default=1,
+        help_text=(
+            'Threshold for checking overlaps between properties (in sqm).'
+        )
+    )
